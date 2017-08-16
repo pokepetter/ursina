a30c944a258cabd0da02ca2ceff46783a25211a7
Petter Amland <pokepetter@gmail.com>
2017-08-16 23:32:22 +0200

    started making move gizmo

---
+++ move_gizmo.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("..")
+from pandaeditor import *
+
+class MoveGizmo():
+
+    def __init__(self):
+        self.entity = None
+        self.axis = None
+
+    def input(self, key):
+        # if key == 'left mouse down' and self.entity.hovered:
+
+        if key == 'left mouse up':
+            print(mouse.delta[0])
+            for t in self.entity.targets:
+                if self.axis == 'x':
+                    t.position_x += mouse.delta[0]
