a8c65bd995ff642837d39f286c5afb49b2526265
Petter Amland <petter@rain-games.com>
2017-11-23 17:16:50 +0100

    reapply texture after changing model.

--- internal_scripts/replace_model_button.py
+++ internal_scripts/replace_model_button.py
@@ -15,8 +15,12 @@ class ReplaceModelButton():
                         s.model.removeAllChildren()
                     except:
                         pass
+                    if s.texture:
+                        self.temp_texture = os.path.basename(s.texture.getFullpath())
                     s.model.removeNode()
                     s.model = os.path.basename(self.path).split('.')[0]
                     s.editor_collider = 'box'
+                    if self.temp_texture:
+                        s.texture = self.temp_texture
 
                 scene.editor.inspector.update_inspector()
