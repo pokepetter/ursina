c78e8f8e2714ddc14ab9433607e03e0db01a9ee1
Petter Amland <pokepetter@gmail.com>
2018-03-01 23:42:48 +0100

    models.

--- pandaeditor/internal_prefabs/models.py
+++ pandaeditor/internal_prefabs/models.py
@@ -8,10 +8,19 @@ class Quad(Entity):
 class Circle(Entity):
     def __init__(self, segments=16):
         super().__init__()
-        self.model = 'circle16'
+        self.model = 'circle_16'
         #gengerate circle16
 
 class Cube(Entity):
     def __init__(self):
         super().__init__()
         self.model = 'cube'
+
+
+# app = PandaEditor()
+# # c = Circle()
+# e = Entity()
+# e.model = 'circle_16'
+# print('----', e.model)
+# # e.rotation_y = 90
+# app.run()
