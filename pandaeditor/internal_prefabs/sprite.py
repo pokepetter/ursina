21254cc86e0625a43428168da55cf160d38fa268
Petter Amland <petter@rain-games.com>
2018-05-16 18:26:51 +0200

    animation class. fixed sprite.

--- pandaeditor/internal_prefabs/sprite.py
+++ pandaeditor/internal_prefabs/sprite.py
@@ -5,11 +5,16 @@ class Sprite(Entity):
         super().__init__(**kwargs)
         self.model = 'quad'
         self.texture = texture
+        if not hasattr(self, 'texture'): #self.texture:
+            print('tolo')
+            destroy(self)
+            return None
+
         self.aspect_ratio = self.model.getTexture().getOrigFileXSize() / self.model.getTexture().getOrigFileYSize()
         self.scale_x = self.scale_y * self.aspect_ratio
 
 
 if __name__ == '__main__':
     app = PandaEditor()
-    s = Sprite('panda_button')
+    s = Sprite('panda_buttons')
     app.run()
