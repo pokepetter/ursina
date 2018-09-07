cc862477c29ef4f292c952ca94d0570a06f46f56
Petter Amland <pokepetter@gmail.com>
2018-09-08 00:04:29 +0200

    turned grid layout into a function.

--- ursina/__init__.py
+++ ursina/__init__.py
@@ -30,6 +30,7 @@ from ursina.internal_prefabs.scrollable import Scrollable
 from ursina.internal_prefabs.grid import Grid
 
 from ursina.internal_scripts.smooth_follow import SmoothFollow
+from ursina.internal_scripts.grid_layout import grid_layout
 
 from ursina.internal_prefabs.debug_menu import DebugMenu
 from ursina.internal_prefabs.memory_counter import MemoryCounter
