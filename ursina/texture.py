1d4d0b105380ebe7158404e467176787bee33a1b
Petter Amland <pokepetter@gmail.com>
2019-04-21 13:39:47 +0200

    fixed bug with pil image and texture. added save function.

--- ursina/texture.py
+++ ursina/texture.py
@@ -13,14 +13,16 @@ except:
 
 class Texture():
 
+    default_filtering = 'bilinear'
+
     def __init__(self, path):
         if 'Image' in str(type(path)):
             # print('passing pil image!')
             image = path
             self._texture = PandaTexture()
             self._texture.setup2dTexture(image.width, image.height, PandaTexture.TUnsignedByte, PandaTexture.FRgba)
-            self._texture.setRamImageAs(image.tobytes(), image.mode)
-            self._cached_image = image
+            self._texture.setRamImageAs(image.transpose(Image.FLIP_TOP_BOTTOM).tobytes(), image.mode)
+            self._cached_image = image.transpose(Image.FLIP_TOP_BOTTOM)
             self.path = None
 
         elif type(path) == PandaTexture:
@@ -32,7 +34,7 @@ class Texture():
 
             self._cached_image = None   # for get_pixel() method
 
-        self.filtering = 'bilinear'
+        self.filtering = Texture.default_filtering
 
 
     @property
@@ -78,6 +80,7 @@ class Texture():
 
     @filtering.setter
     def filtering(self, value):
+        print('setting filtering:', value)
         if value in (None, False, 'nearest', 'nearest neighbor'):
             self._texture.setMagfilter(SamplerState.FT_nearest)
             self._texture.setMinfilter(SamplerState.FT_nearest)
@@ -93,7 +96,7 @@ class Texture():
             if not self._cached_image:
                 self._cached_image = Image.open(self.path)
 
-            col = self._cached_image.getpixel((x, self.height - y -1))
+            col = self._cached_image.getpixel((x, self.height-y-1))
             if len(col) == 3:
                 return (col[0], col[1], col[2], 1)
             else:
@@ -117,11 +120,20 @@ class Texture():
         if not self._cached_image:
             self._cached_image = Image.open(self.path)
 
-        self._cached_image.putpixel((x, y), tuple([int(e*255) for e in color]))
+        self._cached_image.putpixel((x, self.height-y-1), tuple([int(e*255) for e in color]))
 
     def apply(self):
-        self._texture.setRamImageAs(self._cached_image.tobytes(), self._cached_image.mode)
+        if not self._cached_image:
+            self._cached_image = Image.open(self.path)
 
+        self._texture.setRamImageAs(self._cached_image.transpose(Image.FLIP_TOP_BOTTOM).tobytes(), self._cached_image.mode)
+        # self._texture.setRamImageAs(self._cached_image.tobytes(), self._cached_image.mode)
+
+    def save(self, path):
+        if not self._cached_image:
+            self._cached_image = Image.open(self.path)
+
+        self._cached_image.save(path)
 
 if __name__ == '__main__':
     from ursina import *
@@ -134,10 +146,11 @@ if __name__ == '__main__':
         A texture file can be a .png, .jpg or .psd.
         If it's a .psd it and no compressed version exists, it will compress it automatically.
     '''
-
     e = Entity(model='quad', texture='brick')
 
     for y in range(e.texture.height):
         for x in range(e.texture.width):
             if e.texture.get_pixel(x,y) == color.blue:
                 print('found blue pixel at:', x, y)
+
+    app.run()
