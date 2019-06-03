ee0d8a23ee98d5d8653901e462bf118a183a0858
Petter Amland <pokepetter@gmail.com>
2019-06-04 00:23:32 +0200

    rewrote grid_layout.

--- ursina/scripts/grid_layout.py
+++ ursina/scripts/grid_layout.py
@@ -11,40 +11,51 @@ def grid_layout(l, max_x=8, max_y=8, spacing=(0,0,0), origin=(-.5,.5,0), offset=
         return
     x, y, z = 0, 0, 0
 
-    slices = chunk_list(l, max_x * max_y)
-    for s in slices:
-        rows = [e for e in chunk_list(s, max_x)]
-        height = sum([max([e.bounds[1] for e in r]) for r in rows])
-        y = 0
-        for row in rows:
+    dimensions = l[0].bounds
+    direction = [-e*2 for e in origin]
+    direction = [1 if e == 0 else e for e in direction]
+    # print(direction)
+    row = list()
+
+    for i, e in enumerate(l):
+        e.position = (
+            x * dimensions[0] * direction[0],
+            y * dimensions[1] * direction[1],
+            z * dimensions[2] * direction[2]
+        )
+        e.origin = origin
+        row.append(e)
+
+        x += 1
+        if x >= max_x:
+            y += 1
             x = 0
-            width = sum([e.bounds[0] for e in row])
-            for e in row:
-                e.position = (
-                    x + l[0].bounds[0]/2 - width/2 - (width/2 * origin[0]*2),
-                    y + l[0].bounds[1]/2 - height/2 - (height/2 * origin[1]*2),
-                    z + l[0].bounds[2]/2)
+            # center row
+            if origin[0] == 0:
+                for e in row:
+                    e.x -= e.bounds[0] * len(row) / 2 - e.bounds[0] / 2
+            row.clear()
 
-                x += e.bounds[0]
-            y += max([e.bounds[1] for e in row]) + spacing[1]
-        z += max([e.bounds[2] for e in s]) + spacing[2]
+        if y >= max_y:
+            z += 1
+            y = 0
 
-    for e in l:
-        e.z -= z/2 - (z/2 * origin[2]*2) + offset[2]
-        e.y += offset[1]
-        e.x += offset[0]
 
+    # center last row
+    if origin[0] == 0:
+        for e in row:
+            e.x -= e.bounds[0] * len(row) / 2 - e.bounds[0] / 2
 
 if __name__ == '__main__':
     app = Ursina()
 
     center = Entity(model='quad', scale=.1, color=color.red)
     p = Entity()
-    for i in range(32):
-        b = Button(parent=p, model='cube', scale=.5, scale_x=1, text=str(i), color=color.tint(color.random_color(),-.6))
-
+    for i in range(4*5):
+        b = Button(parent=p, model='quad', scale=.5, scale_x=1, text=str(i), color=color.tint(color.random_color(),-.6))
+        b.text_entity.world_scale = 1
     t = time.time()
-    grid_layout(p.children, max_x=4, max_y=3, origin=(-.5, .5))
+    grid_layout(p.children, max_x=7, max_y=10, origin=(0, .5))
     center = Entity(parent=camera.ui, model=Circle(), scale=.005, color=color.lime)
     EditorCamera()
     print(time.time() - t)
