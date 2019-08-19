83c16534a12f4c7ec3779bfa4a2122e747822da4
Petter Amland <pokepetter@gmail.com>
2019-08-19 23:11:39 +0200

    boxcast. added thickness parameter to raycast so I can reuse the same code. will also use CollisionSegment instead of CollisionRay if distance is not infinite.

--- ursina/raycaster.py
+++ ursina/raycaster.py
@@ -4,8 +4,8 @@ from ursina import *
 from ursina.entity import Entity
 from ursina import scene
 # from ursina import render
-from panda3d.core import CollisionTraverser, CollisionNode
-from panda3d.core import CollisionHandlerQueue, CollisionRay
+from panda3d.core import CollisionTraverser, CollisionNode, CollisionHandlerQueue
+from panda3d.core import CollisionRay, CollisionSegment, CollisionBox
 from panda3d.core import Vec3
 import math
 from ursina.hit import Hit
@@ -22,8 +22,6 @@ class Raycaster(Entity):
         self._pq = CollisionHandlerQueue()  # Make a handler
         self._pickerNode = CollisionNode('raycaster')
         self._pickerNP = self.attach_new_node(self._pickerNode)
-        self._collision_ray = CollisionRay()  # Make our ray
-        self._pickerNode.addSolid(self._collision_ray)
         self._picker.addCollider(self._pickerNP, self._pq)
         self._pickerNP.show()
 
@@ -32,12 +30,26 @@ class Raycaster(Entity):
         return math.sqrt(sum( (a - b)**2 for a, b in zip(a, b)))
 
 
-    def raycast(self, origin, direction=(0,0,1), dist=math.inf, traverse_target=scene, ignore=list(), debug=False):
+    def raycast(self, origin, direction=(0,0,1), dist=math.inf, thickness=(0,0), traverse_target=scene, ignore=list(), debug=False):
         self.position = origin
         self.look_at(self.position + direction)
-        # need to do this for it to work for some reason
-        self._collision_ray.set_origin(Vec3(0,0,0))
-        self._collision_ray.set_direction(Vec3(0,1,0))
+        self._pickerNode.clearSolids()
+        if thickness == (0,0):
+            if dist == math.inf:
+                ray = CollisionRay()
+                ray.setOrigin(Vec3(0,0,0))
+                ray.setDirection(Vec3(0,1,0))
+            else:
+                ray = CollisionSegment(Vec3(0,0,0), Vec3(0,dist,0))
+        else:
+            if isinstance(thickness, (int, float, complex)):
+                thickness = (thickness, thickness)
+
+            dist = min(1000, dist)
+            ray = CollisionBox(Vec3(0, dist/2, 0), thickness[0], dist/2, thickness[1])
+
+        self._pickerNode.addSolid(ray)
+
 
         if debug:
             self._pickerNP.show()
@@ -49,6 +61,8 @@ class Raycaster(Entity):
         if self._pq.get_num_entries() == 0:
             self.hit = Hit(hit=False)
             return self.hit
+
+        ignore += tuple([e for e in scene.entities if not e.collision])
 
         self._pq.sort_entries()
         self.entries = [        # filter out ignored entities
@@ -67,6 +81,7 @@ class Raycaster(Entity):
         world_point = self.collision.get_surface_point(render)
         world_point = Vec3(world_point[0], world_point[2], world_point[1])
         hit_dist = self.distance(self.world_position, world_point)
+
         if hit_dist <= dist:
             if nP.name.endswith('.egg'):
                 nP = nP.parent
@@ -80,14 +95,19 @@ class Raycaster(Entity):
             self.hit.point = point
             self.hit.world_point = world_point
             self.hit.distance = hit_dist
+
             normal = self.collision.get_surface_normal(self.collision.get_into_node_path().parent)
             self.hit.normal = (normal[0], normal[2], normal[1])
+
             normal = self.collision.get_surface_normal(render)
             self.hit.world_normal = (normal[0], normal[2], normal[1])
             return self.hit
 
         self.hit = Hit(hit=False)
         return self.hit
+
+
+    self.boxcast = self.raycast
 
 
 sys.modules[__name__] = Raycaster()
@@ -99,7 +119,7 @@ if __name__ == '__main__':
     app = Ursina()
     from ursina.entity import Entity
 
-    d = Entity(parent=scene, position=(0,0,2), model='cube', color=color.red, collider='box')
+    d = Entity(parent=scene, position=(0,0,2), model='cube', color=color.orange, collider='box')
     e = Entity(model='cube', color=color.lime)
 
     camera.position = (0, 15, -15)
@@ -118,11 +138,15 @@ if __name__ == '__main__':
         e.rotation_y -= held_keys['q'] * rotation_speed
         e.rotation_y += held_keys['e'] * rotation_speed
 
-        raycast(e.world_position, e.forward, 3, render, debug=True)
+        ray = raycast(e.world_position, e.forward, 3, thickness=(.1,.2), debug=True)
+        # print(ray.hit)
+        if ray.hit:
+            d.color = color.azure
+        else:
+            d.color = color.orange
 
 
-
-    raycast((0,0,-2), (0,0,1), 5, render, debug=False)
+    raycast((0,0,-2), (0,0,1), 5, debug=False)
 
     EditorCamera()
     app.run()
