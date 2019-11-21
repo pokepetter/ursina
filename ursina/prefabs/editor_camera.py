44f653f5d7ad901edb17ba27c4e33b54cf048f34
Petter Amland <pokepetter@gmail.com>
2019-11-21 14:01:43 +0100

    simplified EditorCamera. removed pivot and will now rotate around mouse hit point.

--- ursina/prefabs/editor_camera.py
+++ ursina/prefabs/editor_camera.py
@@ -5,16 +5,16 @@ class EditorCamera(Entity):
     def __init__(self, **kwargs):
         super().__init__(name='editor_camera', eternal=True)
 
-        self.pivot = Entity(name='pivot', eternal=True)
-        camera.world_parent = self.pivot
-
+        camera.world_parent = self
         self.rotation_speed = 100
         self.pan_speed = (4, 4)
         self.move_speed = 1
         self.zoom_speed = 20
+        self.rotate_around_mouse_hit = True
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
 
 
     def input(self, key):
@@ -25,42 +25,43 @@ class EditorCamera(Entity):
             camera.fov -= 1
             printvar(camera.fov)
 
-        if key == 'p':
+        elif key == 'p':
             camera.orthographic = not camera.orthographic
 
 
-        # zooming, don't zoom if hovering an editor panel
-        if (mouse.left or mouse.right or mouse.middle
-        and mouse.hovered_entity
-        and hasattr(mouse.hovered_entity, 'is_editor')
-        and mouse.hovered_entity.is_editor):     # hovering editor panel
-            pass
-        else:
-            if key == 'scroll up':
-                camera.z += self.zoom_speed
-            if key == 'scroll down':
-                camera.z -= self.zoom_speed
+        elif key == 'scroll up':
+            if mouse.hovered_entity and not mouse.hovered_entity.has_ancestor(camera):
+                camera.world_position = lerp(self.position, mouse.hovered_entity.world_position, .05)
+            else:
+                camera.world_position += camera.forward * self.zoom_speed
 
+        elif key == 'scroll down':
+            camera.world_position += camera.back * self.zoom_speed
+
+
+        elif key == 'right mouse down' or key == 'middle mouse down':
+            if mouse.hovered_entity and self.rotate_around_mouse_hit:
+                org_pos = camera.world_position
+                self.world_position = mouse.world_point
+                camera.world_position = org_pos
 
 
     def update(self):
-        camera.look_at(self.pivot)
-
         if mouse.right:
-            self.pivot.rotation_x -= mouse.velocity[1] * self.rotation_speed
-            self.pivot.rotation_y += mouse.velocity[0] * self.rotation_speed
+            self.rotation_x -= mouse.velocity[1] * self.rotation_speed
+            self.rotation_y += mouse.velocity[0] * self.rotation_speed
 
-            self.pivot.position += camera.right * held_keys['d'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
-            self.pivot.position += camera.left * held_keys['a'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
-            self.pivot.position += camera.forward * held_keys['w'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
-            self.pivot.position += camera.back * held_keys['s'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
-            self.pivot.position += camera.up * held_keys['e'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
-            self.pivot.position += camera.down * held_keys['q'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.right * held_keys['d'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.left * held_keys['a'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.forward * held_keys['w'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.back * held_keys['s'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.up * held_keys['e'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
+            self.position += camera.down * held_keys['q'] * (self.move_speed + (self.move_speed * held_keys['shift']) - (self.move_speed*.9 * held_keys['alt']))
 
 
         if mouse.middle:
-            self.pivot.position -= camera.right * mouse.velocity[0] * self.pan_speed[0]
-            self.pivot.position -= camera.up * mouse.velocity[1] * self.pan_speed[1]
+            self.position -= camera.right * mouse.velocity[0] * self.pan_speed[0]
+            self.position -= camera.up * mouse.velocity[1] * self.pan_speed[1]
 
 
 
@@ -81,6 +82,6 @@ if __name__ == '__main__':
     rotation_info = Text(position=window.top_left)
 
     def update():
-        rotation_info.text = str(int(ec.pivot.rotation_y)) + '\n' + str(int(ec.pivot.rotation_x))
+        rotation_info.text = str(int(ec.rotation_y)) + '\n' + str(int(ec.rotation_x))
 
     app.run()
