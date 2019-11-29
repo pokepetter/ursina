e3ed03656da12c3e60d005d242587d90918b424e
Petter Amland <pokepetter@gmail.com>
2019-11-29 23:25:46 +0100

    fixed ursina splash fade out.

--- ursina/prefabs/ursina_splash.py
+++ ursina/prefabs/ursina_splash.py
@@ -3,11 +3,12 @@ from ursina import *
 if __name__ == '__main__':
     app = Ursina()
 
+window.color = color.yellow
 camera.overlay.fade_in(duration=0)
 logo = Sprite(name='ursina_splash', parent=camera.ui, texture='ursina_logo', world_z=camera.overlay.z-1, scale=.1, color=color.clear)
 logo.animate_color(color.white, duration=1, delay=.0)
 logo.fade_out(.5, delay=5, curve=curve.linear)
-camera.overlay.fade_out(1, delay=5)
+camera.overlay.animate_color(color.clear, duration=1, delay=5)
 destroy(logo, delay=5)
 
 if __name__ == '__main__':
