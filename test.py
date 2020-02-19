6526e6744319ab05bd75beefaf54b98654787083
Clay Boone <tener.hades@gmail.com>
2020-02-19 21:55:39 +0000

    cleanup top level test.py

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 """Test Ursina."""
 
+import logging
+import textwrap
+
+_log = logging.getLogger(__name__)
+
 
 def main():
+    """Run ursina unit tests using pytest.
+
+    Pytest is not a requirement, so in the event that it is not already
+    installed, instruct the user on running tests in an automated fashion
+    using Python's included unittest package.
+    """
     try:
         import pytest
     except ImportError:
-        err = lambda msg: print(f"Error: {msg}")
-        err("You need to install pytest! O:")
+        _log.error(textwrap.dedent(f"""\
+            Pytest is the preferred method of running unit tests. Please run
+            the following command, then re-run {__file__}.
+                python -m pip install pytest
+
+            Otherwise, run the following command from the repository root:
+                python -m unittest discover -s test
+        """))
     else:
         pytest.main()
 
