e06980328f07bda1c489ed00edbf0ab5351d5195
marhone <no7nic3@gmail.com>
2020-02-22 00:13:56 +0800

    try to fix get correct velocity when mouse locked.

--- ursina/mouse.py
+++ ursina/mouse.py
@@ -95,6 +95,8 @@ class Mouse():
             try:
                 object.__setattr__(self, name, value)
                 window.set_cursor_hidden(value)
+                if value:
+                    window.set_mouse_mode(window.M_relative)
                 application.base.win.requestProperties(window)
             except:
                 pass
