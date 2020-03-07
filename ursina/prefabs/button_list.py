beb94d4d4df150440b5937fe7eb42d653425e74c
Petter Amland <pokepetter@gmail.com>
2020-03-07 10:11:04 +0100

    added ButtoonList.button_dict setter so you can update it.

--- ursina/prefabs/button_list.py
+++ ursina/prefabs/button_list.py
@@ -13,24 +13,30 @@ class ButtonList(Entity):
         collider='box',
         )
 
-        self.buttons = button_dict
-
-
-        self.actions = [*self.buttons.values()]
-
-        self.text_entity = Text(parent=self, origin = (-.5,.5), world_scale=20, z=-.1, y=-.25*Text.size, x=.01)
+        self.text_entity = Text(parent=self, origin=(-.5,.5), text='empty', world_scale=20, z=-.1, y=-.25*Text.size, x=.01)
+        self.text_entity.line_height = button_height
         if button_height < 1.5:
             self.text_entity.y += .005
 
-        self.text_entity.text = 'eoifjaofijaoii'
-        self.text_entity.line_height = button_height
         self.button_height = self.text_entity.height
+
+        self.button_dict = button_dict
+        # self.actions = [*self.button_dict.values()] # gets set when setting button_dict
         self.highlight = Entity(parent=self, model='quad', color=color.white33, scale=(1,self.button_height), origin=(-.5,.5), z=-.01)
 
-        self.text_entity.text = '\n'.join(self.buttons.keys())
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+
+    @property
+    def button_dict(self):
+        return self._button_dict
+
+    @button_dict.setter
+    def button_dict(self, value):
+        self._button_dict = value
+        self.actions = [*self.button_dict.values()]
+        self.text_entity.text = '\n'.join(self.button_dict.keys())
 
 
     def on_click(self):
@@ -41,23 +47,24 @@ class ButtonList(Entity):
             action()
 
         elif isinstance(action, Func):
-            action.func(*action.args, **action.kwargs)
-            action.finished = True
+            action.run()
 
         elif isinstance(action, Sequence):
             action.start()
 
         self.highlight.blink(color.black, .1)
-
+        self.enabled = False
 
 
     def update(self):
         self.highlight.enabled = mouse.hovered_entity == self
         if mouse.hovered_entity == self:
-            if abs(mouse.point[1] // self.button_height) > len(self.buttons):
+            if abs(mouse.point[1] // self.button_height) > len(self.button_dict):
                 self.highlight.enabled = False
 
             self.highlight.y = math.ceil(mouse.point[1] / self.button_height) * self.button_height
+
+
 
 if __name__ == '__main__':
     app = Ursina()
@@ -77,6 +84,5 @@ if __name__ == '__main__':
     for i in range(6, 25):
         button_dict[f'button {i}'] = Func(print, i)
 
-    ButtonList(button_dict)
-    # Button('tspogpo').fit()
+    bl = ButtonList(button_dict)
     app.run()
