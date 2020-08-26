fece694b92b49cbd425fe1125bdde8e63c0ecbfe
DiamondsBattle <54222931+DiamondsBattle@users.noreply.github.com>
2020-08-26 15:26:36 +0200

    Added ``DropdownMenuSeparator``
    Added ``DropdownMenuSeparator``.

--- ursina/prefabs/dropdown_menu.py
+++ ursina/prefabs/dropdown_menu.py
@@ -16,6 +16,28 @@ class DropdownMenuButton(Button):
             self.text_entity.origin = (-.5, 0)
             self.text_entity.scale *= .8
 
+class DropdownMenuSeparator(DropdownMenuButton):
+    def __init__(self, **kwargs):
+        super().__init__(
+            on_click=self.on_click,
+            color=color.black,
+            **kwargs
+            )
+        self.setScaleY()
+        self.setColor()
+
+    def setScaleY(self):
+        self.scale_y = .004
+
+    def setColor(self):
+        self.color = color.black
+
+    def update(self):
+        if self.hovered:
+            self.color = color.black
+
+    def on_click(self):
+        self.color = color.black
 
 class DropdownMenu(DropdownMenuButton):
 
@@ -23,22 +45,26 @@ class DropdownMenu(DropdownMenuButton):
         super().__init__(text=text)
         self.position = window.top_left
         self.buttons = buttons
+        previous = None
+        separations = 0
         for i, b in enumerate(self.buttons):
             b.world_parent = self
             b.original_scale = b.scale
             b.x = 0
-            b.y = -i-1 *.98
+            if previous == 'dropdown_menu_separator':
+                separations += 1
+            b.y = (-(i - separations)-1 * .98) + (separations * .006)
             b.enabled = False
+            previous = b.name
 
             if isinstance(b, DropdownMenu):
                 for e in b.buttons:
                     e.x = 1
                     e.y += 1
 
-        self.arrow_symbol = Text(world_parent=self, text='>', origin=(.5,.5), position=(.95, 0), color=color.gray)
+        self.arrow_symbol = Text(world_parent=self, text='>', origin=(.5, .5), position=(.95, 0), color=color.gray)
         for key, value in kwargs.items():
             setattr(self, key, value)
-
 
     def open(self):
         for i, b in enumerate(self.buttons):
@@ -47,7 +73,6 @@ class DropdownMenu(DropdownMenuButton):
     def close(self):
         for i, b in enumerate(reversed(self.buttons)):
             b.enabled = False
-
 
     def on_mouse_enter(self):
         super().on_mouse_enter()
@@ -65,13 +90,12 @@ class DropdownMenu(DropdownMenuButton):
 
 
 if __name__ == '__main__':
-    from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
-
     app = Ursina()
     # DropdownMenu(text='File')
     DropdownMenu('File', buttons=(
         DropdownMenuButton('New'),
         DropdownMenuButton('Open'),
+        DropdownMenuSeparator(),
         DropdownMenu('Reopen Project', buttons=(
             DropdownMenuButton('Project 1'),
             DropdownMenuButton('Project 2'),
