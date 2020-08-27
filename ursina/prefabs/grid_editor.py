cc09a5e8bfa4a4e45128caf5c1b8293456f0ffd2
Petter Amland <pokepetter@gmail.com>
2020-08-27 06:20:02 +0200

    added .edit_mode and save() to GridEditor.

--- ursina/prefabs/grid_editor.py
+++ ursina/prefabs/grid_editor.py
@@ -13,15 +13,18 @@ class GridEditor(Entity):
         self.w, self.h = int(size[0]), int(size[1])
         sys.setrecursionlimit(self.w * self.h)
         # self.grid = [[palette[0] for x in range(self.w)] for y in range(self.h)]
-        self.grid = [[palette[0] for y in range(self.h)] for x in range(self.w)]
+        if not self.grid:
+            self.grid = [[palette[0] for y in range(self.h)] for x in range(self.w)]
         self.brush_size = 1
-        self.cursor = Entity(parent=self, model=Quad(segments=0, mode='line'), origin=(-.5,-.5), scale=(1/self.w, 1/self.h), color=color.color(0,1,1,.5), z=-.1)
+        self.edit_mode = False
+        self.auto_render = True
+        self.cursor = Entity(parent=self, model=Quad(segments=0, mode='line'), origin=(-.5,-.5), scale=(1/self.w, 1/self.h), color=color.color(0,1,1,.5), z=-.1, enabled=self.edit_mode)
 
         self.selected_char = palette[1]
         self.palette = palette
         self.prev_draw = None
         self.start_pos = (0,0)
-        self.auto_render = True
+        self.outline = Entity(parent=self, model=Quad(segments=0, mode='line', thickness=1), color=color.cyan, z=.01, origin=(-.5,-.5), enabled=self.edit_mode)
 
         self.undo_cache = list()
         self.undo_cache.append(deepcopy(self.grid))
@@ -54,7 +57,7 @@ class GridEditor(Entity):
         if hasattr(self, 'palette_parent'):
             destroy(self.palette_parent)
 
-        self.palette_parent = Entity(parent=camera.ui, position=(-.75,-.05))
+        self.palette_parent = Entity(parent=camera.ui, position=(-.75,-.05), enabled=self.edit_mode)
         for i, e in enumerate(value):
             if isinstance(e, str):
                 i = e
@@ -72,6 +75,9 @@ class GridEditor(Entity):
 
 
     def update(self):
+        if not self.edit_mode:
+            return
+
         self.cursor.enabled = mouse.hovered_entity == self
         if self.hovered:
             self.cursor.position = mouse.point
@@ -113,6 +119,15 @@ class GridEditor(Entity):
 
 
     def input(self, key):
+        if key == 'tab':
+            self.edit_mode = not self.edit_mode
+            self.outline.enabled = self.edit_mode
+            self.cursor.enabled = self.edit_mode
+            self.palette_parent.enabled = self.edit_mode
+
+        if not self.edit_mode:
+            return
+
         if key == 'left mouse down':
             self.start_pos = (
                 int(self.cursor.x * (self.w+1)),
@@ -160,6 +175,11 @@ class GridEditor(Entity):
             self.brush_size += 1
             self.cursor.scale = Vec2(self.brush_size / self.w, self.brush_size / self.h)
 
+        if held_keys['control'] and key == 's':
+            if hasattr(self, 'save'):
+                self.save()
+
+
 
     def floodfill(self, matrix, x, y, first=True):
         if matrix[x][y] == self.selected_char:
@@ -185,6 +205,7 @@ class GridEditor(Entity):
 
 class PixelEditor(GridEditor):
     def __init__(self, texture, palette=(color.black, color.white, color.light_gray, color.gray, color.red, color.orange, color.yellow, color.lime, color.green, color.turquoise, color.cyan, color.azure, color.blue, color.violet, color.magenta, color.pink), **kwargs):
+        self.grid = [[texture.get_pixel(x,y) for y in range(texture.height)] for x in range(texture.width)]
         super().__init__(texture=texture, size=texture.size, palette=palette, **kwargs)
         self.texture.filtering = False
         self.render()
@@ -196,6 +217,12 @@ class PixelEditor(GridEditor):
                 self.texture.set_pixel(x, y, self.grid[x][y])
 
         self.texture.apply()
+
+
+    def save(self):
+        if self.texture.path:
+            self.texture.save(self.texture.path)
+            print('saving:', self.texture.path)
 
 
 
@@ -231,14 +258,14 @@ if __name__ == '__main__':
     can be useful for level editors and such
     here we create a new texture, but can also give it an exisitng texture to modify.
     '''
-    from PIL import Image
-    t = Texture(Image.new(mode='RGBA', size=(32,32), color=(0,0,0,1)))
-    PixelEditor(t)
+    # from PIL import Image
+    # t = Texture(Image.new(mode='RGBA', size=(32,32), color=(0,0,0,1)))
+    PixelEditor(texture=load_texture('brick'))
 
     '''
     same as the pixel editor, but with text.
     '''
-    ASCIIEditor()
+    # ASCIIEditor()
 
 
     app.run()
