24e3256df66f96b9d33d7d008bd589a18effd955
Petter Amland <pokepetter@gmail.com>
2020-08-27 06:16:45 +0200

    made duplicate() copy children as well.

--- ursina/duplicate.py
+++ ursina/duplicate.py
@@ -2,8 +2,9 @@ from ursina import *
 from copy import copy, deepcopy
 
 
-def duplicate(entity):
-    e = entity.__class__(add_to_scene_entities=entity in scene.entities)
+def duplicate(entity, copy_children=True, **kwargs):
+    e = entity.__class__(entity.add_to_scene_entities)
+
 
     if hasattr(entity, 'model') and entity.model:
         e.model = copy(entity.model)
@@ -24,8 +25,8 @@ def duplicate(entity):
                 setattr(e, name, getattr(entity, name))
 
     for c in entity.children:
-        clone = duplicate(c)
-        clone.parent = e
+        clone = duplicate(c, copy_children=False)
+        clone.world_parent = e
 
     if 'Audio' in e.types:
         e.volume = entity.volume
@@ -38,26 +39,33 @@ def duplicate(entity):
         e.clip = entity.clip
 
 
+    for key, value in kwargs.items():
+        setattr(e, key ,value)
+
     return e
 
 
 if __name__ == '__main__':
     app = Ursina()
 
-    quad = Button(parent=scene, model='quad', texture='brick', x=-1, collider='box')
-    sprite = Sprite('brick', x=-2, scale=1.5)
-    circle = Entity(model=Circle(6))
-    rounded_quad = Entity(model=Quad(subdivisions=3, mode='line'), x=1)
-    sphere = Entity(model=Sphere(mode='line'), x=2)
-    cone = Entity(model=Cone(), x=3)
-    cone.model.colorize()
-    prismatoid = Entity(model=Prismatoid(), x=4)
-    cylinder = Entity(model=Cylinder(), x=5)
-    mesh = Entity(model=Mesh(vertices=((0,0,0), (0,1,0), (-1,1,0))), x=6)
+    # quad = Button(parent=scene, model='quad', texture='brick', x=-1, collider='box')
+    # sprite = Sprite('brick', x=-2, scale=1.5)
+    # circle = Entity(model=Circle(6))
+    # rounded_quad = Entity(model=Quad(subdivisions=3, mode='line'), x=1)
+    # sphere = Entity(model=Sphere(mode='line'), x=2)
+    # cone = Entity(model=Cone(), x=3)
+    # cone.model.colorize()
+    # prismatoid = Entity(model=Prismatoid(), x=4)
+    # cylinder = Entity(model=Cylinder(), x=5)
+    # mesh = Entity(model=Mesh(vertices=((0,0,0), (0,1,0), (-1,1,0))), x=6)
+    #
+    # for i, e in enumerate((quad, sprite, circle, rounded_quad, sphere, cone, prismatoid, cylinder, mesh)):
+    #     e2 = duplicate(e)
+    #     e2.y += 1.5
 
-    for i, e in enumerate((quad, sprite, circle, rounded_quad, sphere, cone, prismatoid, cylinder, mesh)):
-        e2 = duplicate(e)
-        e2.y += 1.5
-
+    e = Entity(model='cube', collider='box')
+    e.c = Entity(parent=e, model='cube', scale=.5, y=.5, color=color.azure)
+    e2 = duplicate(e, True, x=1.1)
+    # e2.x +=1.1
     EditorCamera()
     app.run()
