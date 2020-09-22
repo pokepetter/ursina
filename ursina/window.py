349d9de73bb1118a78f7705bfad2343198da3cca
Petter Amland <pokepetter@gmail.com>
2020-09-22 22:31:04 +0200

    fixed window resolution for high dpi montiors.

--- ursina/window.py
+++ ursina/window.py
@@ -33,6 +33,7 @@ class Window(WindowProperties):
         if os.name == 'nt':     # windows
             import ctypes
             user32 = ctypes.windll.user32
+            user32.SetProcessDPIAware()
             self.screen_resolution = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
 
         else:
