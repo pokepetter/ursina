3094006372be30ec5aca6b67cbc816ec9d74130c
Petter Amland <pokepetter@gmail.com>
2020-10-28 18:46:51 +0100

    added some info to the minecraft_clone sample so people don't think of it as a scalable solution.

--- samples/minecraft_clone.py
+++ samples/minecraft_clone.py
@@ -1,8 +1,23 @@
+'''
+Disclaimer: This solution is not scalable for creating a big world.
+Creating a game like Minecraft requires specialized knowledge and is not as easy
+to make as it looks.
+
+You'll have to do some sort of chunking of the world and generate a combined mesh
+instead of separate blocks if you want it to run fast. You can use the Mesh class for this.
+
+You can then use blocks with colliders like in this example in a small area
+around the player so you can interact with the world.
+'''
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 
 app = Ursina()
+
+# Define a Voxel class.
+# By setting the parent to scene and the model to 'cube' we it becomes a 3d button.
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
