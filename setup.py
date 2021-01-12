9ff8a5aae44519036f397862bec1565084e68336
Petter Amland <pokepetter@gmail.com>
2021-01-12 18:56:59 +0100

    bumped version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ursina',
-    version='3.2.2',
+    version='3.3.0',
     url='https://github.com/pokepetter/ursina',
     author='Petter Amland',
     author_email='pokepetter@gmail.com',
