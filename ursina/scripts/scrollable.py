25d6e71186b624b6a24369a64e7b4881e5c99d1a
Petter Amland <pokepetter@gmail.com>
2021-02-03 10:46:59 +0100

    refactored Scrollable and added smoothing.

--- ursina/scripts/scrollable.py
+++ ursina/scripts/scrollable.py
@@ -6,36 +6,39 @@ class Scrollable():
         super().__init__()
         self.max = inf
         self.min = -inf
-        self.scroll_speed = .1
-        self.direction = (0,1,0)
+        self.scroll_speed = .05
+        self.scroll_smoothing = 16
+        self.axis = 'y'
+        self.target_value = None
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
 
-    @property
-    def direction(self):
-        return self._direction
 
-    @direction.setter
-    def direction(self, value):
-        value = Vec3(value[0], value[1], value[2] if len(value) == 3 else 0)
-        self._direction = value
+    def update(self):
+        # lerp position
+        if self.target_value:
+            setattr(self.entity, self.axis, lerp(getattr(self.entity, self.axis), self.target_value, time.dt * self.scroll_smoothing))
+
 
 
     def input(self, key):
         if not mouse.hovered_entity:
-            # print('pass', mouse.hovered_entity)
             return
+
+        if not self.target_value:
+            self.target_value = getattr(self.entity, self.axis)
 
         if self.entity.hovered or mouse.hovered_entity.has_ancestor(self.entity):
             # print(key)
             if key == 'scroll up':
-                self.entity.position -= self.direction * self.scroll_speed
+                self.target_value -= self.scroll_speed
             if key == 'scroll down':
-                self.entity.position += self.direction * self.scroll_speed
+                self.target_value += self.scroll_speed
 
-            self.entity.y = max(min(self.entity.y, self.max), self.min)
+
+            self.target_value = max(min(self.target_value, self.max), self.min)
 
 
 if __name__ == '__main__':
@@ -45,6 +48,9 @@ if __name__ == '__main__':
     '''
 
     app = Ursina()
-    p = Panel(scale=(.4, .8), collider='box')
+    p = Button(model='quad', scale=(.4, .8), collider='box')
+    for i in range(8):
+        Button(parent=p , scale_y=.05, text=f'giopwjoigjwr{i}', origin_y=.5, y=.5-(i*.05))
+
     p.add_script(Scrollable())
     app.run()
