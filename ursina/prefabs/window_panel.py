2ce640c6cfa50195ebced6208defcb52cb515416
Petter Amland <pokepetter@gmail.com>
2021-04-04 02:36:34 +0200

    moved laying out elements in WindowPanel into layout() function.

--- ursina/prefabs/window_panel.py
+++ ursina/prefabs/window_panel.py
@@ -23,81 +23,84 @@ class WindowPanel(Draggable):
         if self.text_entity:
             self.text_entity.world_scale_y = 1
 
-        if content:
-            spacing = .25
-            height = 1 + spacing
-
-            if isinstance(content, dict):
-                content = content.values()
-
-            for c in content:
-                # print('........', c)
-                if isinstance(c, Space):
-                    height += c.height
-
-                if isinstance(c, Entity):
-                    c.world_parent = self
-                    c.y = -height
-                    c.z = 0
-
-                    if isinstance(c, InputField):
-                        if self._prev_input_field:
-                            self._prev_input_field.next_field = c
-                        self._prev_input_field = c
-
-                    if isinstance(c, Text):
-                        c.origin = (-.5, .5)
-                        c.x = -.48
-                        height += len(c.lines)
-
-                    elif isinstance(c, Button):
-                        c.world_parent = self
-                        c.scale = (.9, 1)
-                        if hasattr(c, 'height'):
-                            c.scale_y = height
-                        c.model = Quad(aspect=c.world_scale_x/c.world_scale_y)
-                        height += c.scale_y
-                        # c.y -= c.scale_y/2
-
-                    elif isinstance(c, Slider):
-                        c.world_parent = self
-                        c.x = -.5 * .9
-                        c.scale = (.9*2, 20)
-                        print('-------------', c.scale_y * c.height)
-                        height += 1
-
-                    elif hasattr(c, 'scale_y'):
-                        height += c.scale_y
-
-                    if hasattr(c, 'text_entity') and c.text_entity is not None:
-                        c.text_entity.world_scale = (1,1,1)
-
-                    height += spacing
-
-            self.panel = Panel(parent=self, scale_y=height, model=Quad(), origin=(0, .5), z=.1, color=self.color.tint(.1))
-            self.panel.model = Quad(aspect=self.panel.world_scale_x/self.panel.world_scale_y, radius=.025)
-            self.panel.origin = (0, .5)
+        self.panel = Panel(parent=self, origin=(0, .5), z=.1, color=self.color.tint(.1))
 
         if self.popup:
             self.lock_x = True
             self.lock_y = True
-            self.bg = Button(
-                parent=self,
-                z=.1,
-                scale=(999, 999),
-                color=color.black66,
-                highlight_color=color.black66,
-                pressed_color=color.black66,
-                )
-
+            self.bg = Button(parent=self, z=.1, scale=(999, 999), color=color.black66, highlight_color=color.black66, pressed_color=color.black66)
             self.bg.on_click = self.close
+
+        self.layout()
+
+
+    def layout(self):
+        content = self.content
+        if not content:
+            return
+        spacing = .25
+        height = 1 + spacing
+
+        if isinstance(content, dict):
+            content = content.values()
+
+        for c in content:
+            # print('........', c)
+            if isinstance(c, Space):
+                height += c.height
+
+            if isinstance(c, Entity):
+                c.world_parent = self
+                c.y = -height
+                c.z = 0
+
+                if isinstance(c, InputField):
+                    if self._prev_input_field:
+                        self._prev_input_field.next_field = c
+                    self._prev_input_field = c
+
+                if isinstance(c, Text):
+                    c.origin = (-.5, .5)
+                    c.x = -.48
+                    height += len(c.lines)
+
+                elif isinstance(c, Button):
+                    c.world_parent = self
+                    c.scale = (.9, 1)
+                    if hasattr(c, 'height'):
+                        c.scale_y = height
+                    c.model = Quad(aspect=c.world_scale_x/c.world_scale_y)
+                    height += c.scale_y
+                    # c.y -= c.scale_y/2
+
+                elif isinstance(c, Slider):
+                    c.world_parent = self
+                    c.x = -.5 * .9
+                    c.scale = (.9*2, 20)
+                    # print('-------------', c.scale_y * c.height)
+                    height += 1
+
+                elif hasattr(c, 'scale_y'):
+                    height += c.scale_y
+
+                if hasattr(c, 'text_entity') and c.text_entity is not None:
+                    c.text_entity.world_scale = (1,1,1)
+
+                height += spacing
+
+        self.panel.scale_y = height
+        self.panel.model = Quad(aspect=self.panel.world_scale_x/self.panel.world_scale_y, radius=.025)
+        self.panel.origin = (0, .5)
 
 
     def on_enable(self):
-        if self.popup:
-            self.bg.enabled = True
-            self.animate_scale(self._original_scale, duration=.1)
-
+        pass
+        try:
+            if self.popup:
+                self.bg.enabled = True
+                self.animate_scale(self._original_scale, duration=.1)
+        except:
+            pass
 
     def close(self):
         if self.popup:
@@ -111,7 +114,7 @@ if __name__ == '__main__':
     WindowPanel is an easy way to create UI. It will automatically layout the content.
     '''
     app = Ursina()
-    WindowPanel(
+    wp = WindowPanel(
         title='Custom Window',
         content=(
             Text('Name:'),
@@ -127,26 +130,12 @@ if __name__ == '__main__':
             Slider(),
             # ButtonGroup(('test', 'eslk', 'skffk'))
             ),
-            # popup=True
+            popup=True,
+            enabled=False
         )
-    # Text(dedent('''
-    # [        My Window            [x]]
-    # | Create your character
-    # |
-    # | InputField
-    # | [[Male][Female][Other]]
-    # | ButtonGroup(('male', 'female', 'other'))
-    # | Option1:  [t]   Option2: []
-    # |
-    # |[      Submit     ] [ Clear ]
-    # '''[1:]), font='VeraMono.ttf', origin=(0,0)
-    # )
-    # WindowPanel(title='My Window', (
-    #     Text('Enter Name:'),
-    #     InputField('name'),
-    #     (
-    #         Button(text='Submit', on_click='save_system.save(self.parent.input_field.value)'),
-    #         Button(text='Clear', on_click='self.parent.input_field.value='')')
-    #         )
-    #     ))
+
+    def input(key):
+        if key == 'space':
+            wp.enabled = True
+
     app.run()
