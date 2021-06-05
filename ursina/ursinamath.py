a9a2177ddb71845b41da07fbe41037999000a882
Tech Helper <68120313+Tech-Helper503@users.noreply.github.com>
2021-06-05 10:04:12 +0300

    Optimized Fast Inverse Square Root Algorithm

--- ursina/ursinamath.py
+++ ursina/ursinamath.py
@@ -7,16 +7,7 @@ from ursina.color import Color
 internal_sum = sum
 
 def sqrt(number):
-    threehalfs = 1.5
-    x2 = number * 0.5
-    y = c_float(number)
-
-    i = cast(byref(y), POINTER(c_int32)).contents.value
-    i = c_int32(0x5f3759df - (i >> 1))
-    y = cast(byref(i), POINTER(c_float)).contents.value
-
-    y = y * (1.5 - (x2 * y * y))
-    return y
+    return number ** -0.5
 
 
 
