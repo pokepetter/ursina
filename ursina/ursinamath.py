628d40a459f7c84c3d187832b4501d5279585734
Petter Amland <pokepetter@gmail.com>
2021-06-05 10:58:36 +0200

    added slerp().

--- ursina/ursinamath.py
+++ ursina/ursinamath.py
@@ -1,5 +1,5 @@
 import operator
-from math import sqrt
+from math import sqrt, sin, acos
 from panda3d.core import Vec4, LVector3f, Mat3, Mat4
 from ursina.vec2 import Vec2
 from ursina.vec3 import Vec3
@@ -65,6 +65,27 @@ def lerp(a, b, t):
 
 def inverselerp(a, b, t) :
     return (a - b) / (t - b)
+
+
+def slerp(q1, q2, t):
+    costheta = q1.dot(q2)
+    if costheta < 0.0:
+        costheta = -costheta
+        q1 = q1.conjugate()
+    elif costheta > 1.0:
+        costheta = 1.0
+
+    theta = acos(costheta)
+    if abs(theta) < 0.01:
+        return q2
+
+    sintheta = sqrt(1.0 - costheta * costheta)
+    if abs(sintheta) < 0.01:
+        return (q1+q2)*0.5
+
+    r1 = sin((1.0 - t) * theta) / sintheta
+    r2 = sin(t * theta) / sintheta
+    return (q1*r1) + (q2*r2)
 
 
 def clamp(value, floor, ceiling):
