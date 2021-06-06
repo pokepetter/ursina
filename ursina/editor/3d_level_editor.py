a02c183804a8ba14ee11f16833ccd32772c8ede3
Petter Amland <pokepetter@gmail.com>
2021-06-06 22:39:23 +0200

    added quick scaling on x, y and z to level editor. hold s while pressing x, y or z.

--- ursina/editor/3d_level_editor.py
+++ ursina/editor/3d_level_editor.py
@@ -13,6 +13,11 @@ class LevelEditor(Entity):
         self.origin_mode = 'center'
         self.origin_mode_menu = ButtonGroup(['last', 'center', 'individual'], min_selection=1, position=window.top)
         self.editor_camera = EditorCamera(parent=self)
+        self.ui = Entity(parent=camera.ui)
+        self.on_enable = Func(self.ui.enable)
+        self.on_disable = Func(self.ui.disable)
+
+
 
 level_editor = LevelEditor()
 
@@ -129,12 +134,14 @@ class ScaleGizmo(Draggable):
         self.scaler = Entity(parent=gizmo)
         self.axis = Vec3(1,1,1)
         self.on_click = Func(setattr, self, 'axis', Vec3(1,1,1))
+        self.arrows = {}
 
         for i, dir in enumerate((Vec3(1,0,0), Vec3(0,1,0), Vec3(0,0,1))):
             b = Button(parent=self, model='scale_gizmo', origin_z=-.5, scale=4, collider='box',
                 color=axis_colors[('x','y','z')[i]], is_gizmo=True, always_on_top=True, render_queue=1, shader=unlit_shader,
                 on_click=Sequence(Func(setattr, self, 'axis', dir), Func(self.drag)))
             b.look_at(dir)
+            self.arrows['xyz'[i]] = b
 
 
     def drag(self):
@@ -186,49 +193,67 @@ class QuickGrabber(Entity):
     def __init__(self, **kwargs):
         super().__init__(
             parent=level_editor,
-            mappings={
+            gizmos_to_toggle={
                 'g' : gizmo.arrows['xz'],
                 'x' : gizmo.arrows['x'],
                 'y' : gizmo.arrows['y'],
                 'z' : gizmo.arrows['z'],
                 's' : scale_gizmo,
+                'sx' : scale_gizmo,
+                'sy' : scale_gizmo,
+                'sz' : scale_gizmo,
+
             },
             clear_selection = False
             )
 
     def input(self, key):
+        if held_keys['s'] and not key == 's':
+            key = 's' + key
+
         if key in ('g', 'x', 'y', 'z'):
             self.original_gizmo_state = gizmo_toggler.animator.state
             gizmo_toggler.animator.state = 'w'
-        elif key == 's':
+
+        elif key in ('s', 'sx', 'sy', 'sz'):
             self.original_gizmo_state = gizmo_toggler.animator.state
             gizmo_toggler.animator.state = 'e'
+
+            if not key == 's':
+                scale_gizmo.axis = (Vec3(1,0,0), Vec3(0,1,0), Vec3(0,0,1))[('sx', 'sy', 'sz').index(key)]
+
         # elif key == 'r':
         #     gizmo_toggler.state = 'r'
 
-        if key in self.mappings.keys() and not held_keys['control']:
+        if key in self.gizmos_to_toggle.keys() and not held_keys['control']:
             gizmo.arrow_parent.visible = False
-            self.mappings[key].visible_self = False
-            self.clear_selection = not level_editor.selection
+            scale_gizmo.visible = False
+            self.gizmos_to_toggle[key].visible_self = False
+            if not key in ('sx', 'sy', 'sz'):
+                self.clear_selection = not level_editor.selection
 
             if not level_editor.selection:
                 selector.input('left mouse down')
             elif gizmo_toggler.animator.state == 'w':
-                mouse.position = self.mappings[key].screen_position
+                mouse.position = self.gizmos_to_toggle[key].screen_position
 
-            self.mappings[key].input('left mouse down')
-            invoke(self.mappings[key].start_dragging, delay=1/60)
+            self.gizmos_to_toggle[key].input('left mouse down')
+            invoke(self.gizmos_to_toggle[key].start_dragging, delay=1/60)
 
-        if key.endswith(' up') and key[:-3] in self.mappings.keys():
+        if key.endswith(' up') and key[:-3] in self.gizmos_to_toggle.keys():
             key = key[:-3]
-            self.mappings[key].input('left mouse up')
+            self.gizmos_to_toggle[key].input('left mouse up')
             if self.clear_selection:
                 level_editor.selection.clear()
                 selector.render_selection()
 
             gizmo.arrow_parent.visible = True
-            self.mappings[key].visible_self = True
+            scale_gizmo.visible = True
+            scale_gizmo.axis = Vec3(1,1,1)
+            self.gizmos_to_toggle[key].visible_self = True
             gizmo_toggler.animator.state = self.original_gizmo_state
+
+
 
 QuickGrabber(parent=level_editor)
 
@@ -262,7 +287,6 @@ class Selector(Entity):
                 level_editor.selection = [clicked_entity, ]   # overwrite
 
         if held_keys['alt'] and clicked_entity in level_editor.selection:
-            print('rrrrrrrrrrrrrr')
             level_editor.selection.remove(clicked_entity) # remove
             # return
 
@@ -291,7 +315,7 @@ class Selector(Entity):
         # gizmo.enabled = False
 
 
-t = Text(position=window.top_left)
+t = Text(position=window.top_left + Vec2(.01,-.06))
 def update():
     t.text = 'selection:\n' + '\n'.join([str(e) for e in level_editor.selection])
 
@@ -353,7 +377,7 @@ class SelectionBox(Entity):
             self.scale_x = mouse.x - self.x
             self.scale_y = mouse.y - self.y
 
-SelectionBox(parent=camera.ui, model=Quad(0, mode='line'), origin=(-.5,-.5,0), scale=(0,0,1), color=color.white33, mode='new')
+SelectionBox(parent=level_editor.ui, model=Quad(0, mode='line'), origin=(-.5,-.5,0), scale=(0,0,1), color=color.white33, mode='new')
 
 
 class Spawner(Entity):
@@ -386,7 +410,7 @@ Spawner(parent=level_editor)
 class PointOfViewSelector(Entity):
     def __init__(self, **kwargs):
 
-        super().__init__(parent=camera.ui, model='cube', collider='box', texture='white_cube', scale=.05, position=window.top_right-Vec2(.1,.1))
+        super().__init__(parent=level_editor.ui, model='cube', collider='box', texture='white_cube', scale=.05, position=window.top_right-Vec2(.1,.1))
         self.front_text = Text(parent=self, text='front', z=-.5, scale=10, origin=(0,0), color=color.azure)
 
         for key, value in kwargs.items():
@@ -411,10 +435,75 @@ class PointOfViewSelector(Entity):
         elif key == '5': camera.orthographic = not camera.orthographic
 
 
+camera.clip_plane_near = 2
+camera.clip_plane_far = 200
+from ursina.shaders import ssao_shader
+camera.shader = ssao_shader
 
 
 PointOfViewSelector()
 
+class AssetBrowser(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(parent=level_editor)
+        self.menu = Entity(parent=level_editor.ui, model=Quad(radius=.05), color=color.black, scale=.2, origin=(.5,0), x=camera.aspect_ratio*.495, collider='box')
+        self.menu.grid = Entity(parent=self.menu, model=Grid(8,8), z=-1, origin=self.menu.origin, color=color.dark_gray)
+        self.cursor = Entity(parent=self.menu, scale=1/8, model='quad', color=color.azure, origin=(-.5,-.5))
+        self.tabs = [Button(parent=self.menu, scale=(1/4,1/8), position=(-1+(i/4),.5), origin=(-.5,-.5), color=color.hsv(90*i,.5,.3)) for i in range(4)]
 
+    def update(self):
+        self.cursor.enabled = self.menu.hovered
+        if self.menu.hovered:
+            grid_pos = [floor(mouse.point.x * 8) / 8, floor(mouse.point.y * 8) / 8]
+            self.cursor.position = grid_pos
+
+
+    def input(self, key):
+        if key == 'm':
+            self.menu.enabled = not self.menu.enabled
+
+        if key == 'left mouse down' and self.menu.hovered:
+
+            if not held_keys['shift'] and not held_keys['alt']:
+                print('go to level')
+
+            if held_keys['shift'] and not held_keys['alt']:
+                print('add level')
+
+            if held_keys['alt'] and not held_keys['shift']:
+                print('unload level')
+
+
+    # def on_click(self):
+    #     print('a')
+    #     self.cursor.position = mouse.point
+
+
+AssetBrowser()
+
+class Help(Button):
+    def __init__(self, **kwargs):
+        super().__init__(parent=level_editor.ui, text='?', scale=.025, model='circle', origin=(-.5,.5), position=window.top_left)
+        self.tooltip = Text(
+            position=self.position + Vec3(.05,-.05,-1),
+            # wordwrap=0,
+            font='VeraMono.ttf',
+            enabled=False,
+            text=dedent('''
+                Hotkeys:
+                n:          add new cube
+                w:          move tool
+                g:          quick move
+                x/y/z:      hold to quick move on axis
+                e:          scale tool
+                s:          quick scale
+                s + x/y/z:  quick scale on axis
+                f:          move the point the camera rotates around to target
+                shift+f:    reset the point camera rotates around to (0,0,0)
+            ''').strip(),
+            background=True
+        )
+
+Help()
 
 app.run()
