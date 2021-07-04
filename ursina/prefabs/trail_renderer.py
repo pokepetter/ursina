b3a3d6f733df69a690e26f8a59482ffe5f99bb71
Petter Amland <pokepetter@gmail.com>
2021-07-04 21:42:59 +0200

    made TrailRenderer use parent instead of target. also added length, color and end_color variables.

--- ursina/prefabs/trail_renderer.py
+++ ursina/prefabs/trail_renderer.py
@@ -1,19 +1,17 @@
 from ursina import *
 
 class TrailRenderer(Entity):
-    def __init__(self, target=None, **kwargs):
-        super().__init__()
-        self.model = Mesh(
-            vertices=[Vec3(0,0,0) for i in range(6)],
-            colors=[lerp(color.clear, color.white, i/6*2) for i in range(6)],
+    def __init__(self, thickness=10, color=color.white, end_color=color.clear, length=6, **kwargs):
+        super().__init__(**kwargs)
+        self.renderer = Entity(
+            model = Mesh(
+            vertices=[self.world_position for i in range(length)],
+            colors=[lerp(end_color, color, i/length*2) for i in range(length)],
             mode='line',
-            thickness=5,
+            thickness=thickness,
             static=False
+            )
         )
-        self.target = target
-        if not target:
-            self.target = self
-
         self._t = 0
         self.update_step = .025
 
@@ -22,18 +20,39 @@ class TrailRenderer(Entity):
         self._t += time.dt
         if self._t >= self.update_step:
             self._t = 0
-            self.model.vertices.pop(0)
-            self.model.vertices.append(self.target.world_position)
-            self.model.generate()
+            self.renderer.model.vertices.pop(0)
+            self.renderer.model.vertices.append(self.world_position)
+            self.renderer.model.generate()
+
+    def on_destroy(self):
+        destroy(self.renderer)
+
 
 
 if __name__ == '__main__':
     app = Ursina()
+    window.color = color.black
     mouse.visible = False
-    player = Entity(model='cube', scale=.1, color=color.orange)
-    trail_renderer = TrailRenderer(target=player)
+    player = Entity()
+    player.graphics = Entity(parent=player, scale=.1, model='circle')
+    trail_renderer = TrailRenderer(parent=player, thickness=100, color=color.yellow, length=6)
+
+    pivot = Entity(parent=player)
+    trail_renderer = TrailRenderer(parent=pivot, x=.1, thickness=20, color=color.orange)
+    trail_renderer = TrailRenderer(parent=pivot, y=1, thickness=20, color=color.orange)
+    trail_renderer = TrailRenderer(parent=pivot, thickness=2, color=color.orange, alpha=.5, position=(.4,.8))
+    trail_renderer = TrailRenderer(parent=pivot, thickness=2, color=color.orange, alpha=.5, position=(-.5,.7))
 
     def update():
-        player.position = mouse.position * 10
+        player.position = lerp(player.position, mouse.position*10, time.dt*4)
+
+        if pivot:
+            pivot.rotation_z -= 3
+            pivot.rotation_x -= 2
+
+    def input(key):
+        if key == 'space':
+            destroy(pivot)
+
 
     app.run()
