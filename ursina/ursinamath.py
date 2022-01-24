519afb1c3fd9c46944c29b3e0cb7c43c05462e9a
Werxzy <Werxzyz@yahoo.com>
2022-01-24 02:44:53 -0600

    Added math functions, that can be used for multiple data types, for vector magnitude, dot product, cross product, reflection, normalization, sign, and approaching functions for floats, vectors, and angles. Included examples for the functions.

--- ursina/ursinamath.py
+++ ursina/ursinamath.py
@@ -42,6 +42,45 @@ def distance_xz(a, b):
     return sqrt((b[0] - a[0])**2 + (b[2] - a[2])**2)
 
 
+def magnitude(a):
+    return sqrt(sum(a[i]**2 for i in range(len(a))))
+
+def dot(a, b):
+    return sum(a[i]*b[i] for i in range(len(a)))
+
+def cross(a, b):
+    if len(a) == 2:
+        return a[0]*b[1] - a[1]*b[0]
+    
+    if len(a) == 3:
+        if isinstance(a, (list, tuple)):
+            return [a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]]
+        else:
+            return Vec3(a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0])
+    return a
+
+def reflect(a, normal):
+    return a - normal * dot(a, normal) * 2
+
+def normalize(a):
+    mag = magnitude(a)
+    if isinstance(a, tuple):
+        a = list(a)
+    for i in range(len(a)):
+        a[i] /= mag
+    return a
+
+def sign(a):
+    if isinstance(a, (int, float)):
+        return 1 if a > 0 else -1
+    if isinstance(a, (list, tuple)):
+        return [1 if a[i] > 0 else -1 for i in range(len(a))]
+    if isinstance(a, (Vec2, Vec3, Vec4, LVector3f)):
+        for i in range(len(a)):
+            a[i] = 1 if a[i] > 0 else -1
+    return a
+
+
 def lerp(a, b, t):
     if isinstance(a, (int, float, complex)):
         return a + (b - a) * t
@@ -87,6 +126,42 @@ def slerp(q1, q2, t):
     r2 = sin(t * theta) / sintheta
     return (q1*r1) + (q2*r2)
 
+
+def move_towards(a, b, amount):
+    if isinstance(a, (Vec2, Vec3, Vec4, LVector3f)):
+        delta = b - a
+        delta_length = magnitude(delta)
+        if delta_length < amount:
+            return b
+        return a + delta / delta_length * amount 
+    
+    if isinstance(a, (list, tuple)):
+        size = range(len(a))
+        delta = [(b[i] - a[i]) for i in size]
+        delta_length = magnitude(delta)
+        if delta_length < amount:
+           return a
+        m = min(delta_length, amount) / delta_length
+        return [a[i] + delta[i] * m for i in size]
+    
+    if isinstance(a, (int, float)):
+        if abs(b - a) < amount:
+            return b
+        return a + (amount if a < b else -amount)
+    
+    return a
+
+
+def move_towards_angle(a, b, amount):
+    if not isinstance(a, (int, float)):
+        return a
+    a, b = a % 360, b % 360
+
+    if abs(b - a) > 180:
+        b += 360 if a > b else -360
+    if abs(b - a) < amount:
+        return b
+    return a + (amount if b > a else -amount)
 
 def clamp(value, floor, ceiling):
     return max(min(value, ceiling), floor)
@@ -145,5 +220,30 @@ if __name__ == '__main__':
     print(lerp([0,0], [0,1], .5))
 
     print(round(Vec3(.38, .1351, 353.26), 2))
+
+    print(move_towards(Vec2(0,0), Vec2(1,-1), 1))
+    print(move_towards((0,0,2), (2,1,0), 1))
+    print(move_towards_angle(340, 20, 10))
+
+    print(magnitude(Vec3(1,2,3)))
+    print(normalize(Vec3(1,2,3)))
+    print(dot(Vec3(1,1,0), Vec3(0,1,1)))
+    print(cross((3,1,3), (2,2,1)))
+    
+    print(sign(-99))
+    print(sign(Vec3(1,-2,-3)))
+
+    e3 = Entity(model = 'cube', position = (-3,1,0))
+    target = Vec3(-3,1,1)
+
+    window.borderless = False
+    def update():
+        global velocity
+        e3.position = move_towards(e3.position, target, 5 * time.dt)
+        
+    def input(key):
+        if key ==  'space':
+            global target
+            target = -target
 
     app.run()
