990d42505d5d0b4c364e9443e67c01ac4af827a4
Petter Amland <pokepetter@gmail.com>
2022-01-31 01:24:55 +0100

    fixed procedural cone.

--- ursina/models/procedural/cone.py
+++ ursina/models/procedural/cone.py
@@ -1,48 +1,44 @@
-from ursina import *
+from ursina import Mesh, Vec3, rotate_point_2d
 
 
 class Cone(Mesh):
-    def __init__(self, resolution=4, radius=.5, height=1, direction=(0,1,0), add_bottom=True, mode='triangle', **kwargs):
-        origin = Entity()
-        point = Entity(parent=origin)
+    def __init__(self, resolution=4, radius=.5, height=1, add_bottom=True, mode='triangle', **kwargs):
 
-        axis_index = direction.index(1)
-        right = axis_index+1
-        if right >= 3:
-            right = 0
-        setattr(point, ('x', 'y', 'z')[right], radius)
+        v = Vec3(radius, 0, 0)
+        origin = Vec3(0,0,0)
+        degrees_to_rotate = 360 / resolution
 
-        rot_axis = 'rotation_' + ('x', 'y', 'z')[axis_index]
-
-        verts = list()
+        verts = []
         for i in range(resolution):
-            verts.append(point.world_position) # outer point
-            if add_bottom:
-                verts.append((0,0,0)) #center point
+            verts.append(Vec3(v[0], 0, v[1]))
+            v = rotate_point_2d(v, origin, -degrees_to_rotate)
+            verts.append(Vec3(v[0], 0, v[1]))
 
-            setattr(origin, rot_axis, getattr(origin, rot_axis) - 360/resolution)
-            verts.append(point.world_position) # next outer point
+            verts.append(Vec3(0,height,0))
+        if add_bottom:
+            for i in range(resolution):
+                verts.append(Vec3(v[0], 0, v[1]))
+                verts.append(Vec3(0,0,0))
+                v = rotate_point_2d(v, origin, -degrees_to_rotate)
+                verts.append(Vec3(v[0], 0, v[1]))
 
-            if add_bottom:
-                setattr(origin, rot_axis, getattr(origin, rot_axis) - 360/resolution)
 
-            verts.append((height*direction[0], height*direction[1], height*direction[2])) # spike point
-
-            if add_bottom:
-                verts.append(point.world_position)
-                setattr(origin, rot_axis, getattr(origin, rot_axis) - 360/resolution)
-                verts.append(point.world_position)
-
-        destroy(origin)
-        destroy(point)
-
-        super().__init__(vertices=verts, mode=mode, **kwargs)
+        super().__init__(vertices=verts, uvs=[e.xy for e in verts], mode=mode, **kwargs)
 
 
 if __name__ == '__main__':
+    from ursina import Ursina, Entity, color, EditorCamera
     app = Ursina()
-    # Entity(model=Cone(8), color=color.color(60,1,1,.3))
-    Entity(model=Cone(8, direction=(0,1,0)), color=color.color(60,1,1,.3))
+    e = Entity(model=Cone(3), texture='brick')
+
+    # # rotate model
+    # for i, v in enumerate(e.model.vertices):
+    #     x, y = rotate_point_2d((v.x, v.y), (0,0), 90)
+    #
+    #     e.model.vertices[i] = Vec3(x, y, v.z)
+    #
+    # e.model.generate()
+
     origin = Entity(model='quad', color=color.orange, scale=(.05, .05))
-    ed = EditorCamera(rotation_speed = 200, panning_speed=200)
+    ed = EditorCamera()
     app.run()
