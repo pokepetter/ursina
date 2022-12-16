c68d8007b522d756d8084fa032a5cbc1b446368e
Petter Amland <petter@rain-games.com>
2022-12-16 17:09:40 +0100

    refactored cheap physics example to use classes.

--- samples/cheap_physics.py
+++ samples/cheap_physics.py
@@ -1,11 +1,35 @@
 from ursina import *
 
 
-app = Ursina(vsync=False, size=(1280,720))
+app = Ursina(size=(1280,720))
 
 physics_entities = []
-walls = []
+class PhysicsEntity(Entity):
+    def __init__(self, model='cube', collider='box', **kwargs):
+        super().__init__(model=model, collider=collider, **kwargs)
+        physics_entities.append(self)
 
+    def update(self):
+        if self.intersects():
+            self.stop()
+            return
+
+        self.velocity = lerp(self.velocity, Vec3(0), time.dt)
+        self.velocity += Vec3(0,-1,0) * time.dt * 5
+        self.position += (self.velocity + Vec3(0,-4,0)) * time.dt
+
+
+    def stop(self):
+        self.velocity = Vec3(0,0,0)
+        if self in physics_entities:
+            physics_entities.remove(self)
+
+    def on_destroy(self):
+        self.stop()
+
+
+    def throw(self, direction, force):
+        pass
 
 from ursina.shaders import lit_with_shadows_shader
 Entity.default_shader = lit_with_shadows_shader
@@ -16,23 +40,11 @@ ground = Entity(model='plane', scale=32, texture='white_cube', texture_scale=Vec
 from ursina.prefabs.first_person_controller import FirstPersonController
 player = FirstPersonController()
 
-walls.append(ground)
-
 def input(key):
     if key == 'left mouse down':
-        e = Entity(model='cube', color=color.azure, velocity=Vec3(0), position=player.position+Vec3(0,1.5,0)+player.forward, collider='box')
+        e = PhysicsEntity(model='cube', color=color.azure, velocity=Vec3(0), position=player.position+Vec3(0,1.5,0)+player.forward, collider='sphere')
         e.velocity = (camera.forward + Vec3(0,.5,0)) * 10
-        physics_entities.append(e)
-
-def update():
-    for e in physics_entities:
-        if e.intersects():
-            e.velocity = lerp(e.velocity, Vec3(0), time.dt*10)
-            continue
-
-        e.velocity = lerp(e.velocity, Vec3(0), time.dt)
-        e.velocity += Vec3(0,-1,0) * time.dt * 5
-        e.position += (e.velocity + Vec3(0,-4,0)) * time.dt
+        # physics_entities.append(e)
 
 
 Sky()
