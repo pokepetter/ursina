6e546396863e2b1993fa451890edc8fb28ca47ae
Petter Amland <petter@rain-games.com>
2023-03-10 18:42:22 +0100

    fixed typo.

--- ursina/prefabs/sprite_sheet_animation.py
+++ ursina/prefabs/sprite_sheet_animation.py
@@ -34,7 +34,7 @@ class SpriteSheetAnimation(Entity):
 
 if __name__ == '__main__':
     '''
-    Sprite sheet coorinate system:
+    Sprite sheet coordinate system:
     (0,3) (1,3) (2,3) (3,3)
     (0,2) (1,2) (2,2) (3,2)
     (0,1) (1,1) (2,1) (3,1)
