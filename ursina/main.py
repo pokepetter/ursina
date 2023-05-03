4e0238ecebf714e4aeaa63d49e122de54fe376c0
tanner <tannnxr@gmail.com>
2023-05-03 06:19:52 -0400

    Added documentation for the Ursina singleton

--- ursina/main.py
+++ ursina/main.py
@@ -13,32 +13,72 @@ from ursina.camera import instance as camera
 from ursina.mouse import instance as mouse
 from ursina.string_utilities import print_info
 
+from ursinatypes import *
+
+from typing import Dict
+
 
 import __main__
+
 time.dt = 0
-keyboard_keys = '1234567890qwertyuiopasdfghjklzxcvbnm'
+keyboard_keys = "1234567890qwertyuiopasdfghjklzxcvbnm"
+
 
 def singleton(cls, **kwargs):
     instances = {}
+
     def getinstance(**kwargs):
         if cls not in instances:
             instances[cls] = cls(**kwargs)
         return instances[cls]
+
     return getinstance
+
 
 @singleton
 class Ursina(ShowBase):
-    def __init__(self, **kwargs): # optional arguments: title, fullscreen, size, forced_aspect_ratio, position, vsync, borderless, show_ursina_splash, render_mode, development_mode, editor_ui_enabled, window_type='onscreen'/'offscreen'/'none'.
-        for name in ('title', 'size', 'vsync', 'forced_aspect_ratio'):
+    def __init__(
+        self, **kwargs # I couldn't figure out how to type-hint this. Please don't make fun of me.
+    ) -> Ursina:
+        """
+        Summary:
+            The main class of the Ursina Engine. It is a singleton, so you can only have one instance of it. It is responsible for creating the window, running the game loop, and handling the main scene.
+        
+        Inherits:
+            ShowBase
+
+        Args:
+
+            (None of these args actually default to what they claim to default to. They are just here for the future!!!)
+
+            title (OptionalString): The title of the window. Defaults to "Ursina Engine".\n
+            window_size (WindowSize): The size of the window. Defaults to (1280, 720).\n
+            vertical_sync (OptionalBool): Whether or not to use vertical sync. Defaults to True.\n
+            forced_aspect_ratio (OptionalBool): Whether or not to force the aspect ratio. Defaults to False.\n
+            fullscreen (OptionalBool): Whether or not to set the window to the full size of the screen. Defaults to False.\n
+            borderless (OptionalBool): Whether or not to make the window borderless. Defaults to False.\n
+            show_ursina_splash (OptionalBool): Whether or not to show the Ursina splash screen. Defaults to False.\n
+            render_mode (RenderMode): The render mode. Defaults to "default".\n
+            window_type (WindowType): The type of window. Defaults to "onscreen".\n
+            development_mode (OptionalBool): Whether or not to enable development mode. Defaults to False.\n
+            editor_ui_enabled (OptionalBool): Whether or not to enable the editor UI. Defaults to False.\n
+
+        Returns:
+            Ursina: The Ursina instance.
+
+        Aliases:
+            None.
+        """
+        for name in ("title", "size", "vsync", "forced_aspect_ratio"):
             if name in kwargs and hasattr(window, name):
                 setattr(window, name, kwargs[name])
 
-        if 'development_mode' in kwargs:
-            application.development_mode = kwargs['development_mode']
+        if "development_mode" in kwargs:
+            application.development_mode = kwargs["development_mode"]
 
-        application.window_type = 'onscreen'
-        if 'window_type' in kwargs:
-            application.window_type = kwargs['window_type']
+        application.window_type = "onscreen"
+        if "window_type" in kwargs:
+            application.window_type = kwargs["window_type"]
         # application.window_type = window_type
 
         application.base = self
@@ -46,17 +86,24 @@ class Ursina(ShowBase):
 
         try:
             import gltf
+
             gltf.patch_loader(self.loader)
         except:
             pass
 
         window.late_init()
-        for name in ('fullscreen', 'position', 'show_ursina_splash', 'borderless', 'render_mode'):
+        for name in (
+            "fullscreen",
+            "position",
+            "show_ursina_splash",
+            "borderless",
+            "render_mode",
+        ):
             if name in kwargs and hasattr(window, name):
                 setattr(window, name, kwargs[name])
 
         # camera
-        if application.window_type != 'none':
+        if application.window_type != "none":
             camera._cam = self.camera
             camera._cam.reparent_to(camera)
             camera.render = self.render
@@ -65,33 +112,65 @@ class Ursina(ShowBase):
             camera.set_up()
 
         # input
-        if application.window_type == 'onscreen':
-            self.buttonThrowers[0].node().setButtonDownEvent('buttonDown')
-            self.buttonThrowers[0].node().setButtonUpEvent('buttonUp')
-            self.buttonThrowers[0].node().setButtonRepeatEvent('buttonHold')
-            self.buttonThrowers[0].node().setKeystrokeEvent('keystroke')
+        if application.window_type == "onscreen":
+            self.buttonThrowers[0].node().setButtonDownEvent("buttonDown")
+            self.buttonThrowers[0].node().setButtonUpEvent("buttonUp")
+            self.buttonThrowers[0].node().setButtonRepeatEvent("buttonHold")
+            self.buttonThrowers[0].node().setKeystrokeEvent("keystroke")
         self._input_name_changes = {
-            'mouse1' : 'left mouse down', 'mouse1 up' : 'left mouse up', 'mouse2' : 'middle mouse down', 'mouse2 up' : 'middle mouse up', 'mouse3' : 'right mouse down', 'mouse3 up' : 'right mouse up',
-            'wheel_up' : 'scroll up', 'wheel_down' : 'scroll down',
-            'arrow_left' : 'left arrow', 'arrow_left up' : 'left arrow up', 'arrow_up' : 'up arrow', 'arrow_up up' : 'up arrow up', 'arrow_down' : 'down arrow', 'arrow_down up' : 'down arrow up', 'arrow_right' : 'right arrow', 'arrow_right up' : 'right arrow up',
-            'lcontrol' : 'left control', 'rcontrol' : 'right control', 'lshift' : 'left shift', 'rshift' : 'right shift', 'lalt' : 'left alt', 'ralt' : 'right alt',
-            'lcontrol up' : 'left control up', 'rcontrol up' : 'right control up', 'lshift up' : 'left shift up', 'rshift up' : 'right shift up', 'lalt up' : 'left alt up', 'ralt up' : 'right alt up',
-            'control-mouse1' : 'left mouse down', 'control-mouse2' : 'middle mouse down', 'control-mouse3' : 'right mouse down',
-            'shift-mouse1' : 'left mouse down', 'shift-mouse2' : 'middle mouse down', 'shift-mouse3' : 'right mouse down',
-            'alt-mouse1' : 'left mouse down', 'alt-mouse2' : 'middle mouse down', 'alt-mouse3' : 'right mouse down',
-            'page_down' : 'page down', 'page_down up' : 'page down up', 'page_up' : 'page up', 'page_up up' : 'page up up',
-            }
+            "mouse1": "left mouse down",
+            "mouse1 up": "left mouse up",
+            "mouse2": "middle mouse down",
+            "mouse2 up": "middle mouse up",
+            "mouse3": "right mouse down",
+            "mouse3 up": "right mouse up",
+            "wheel_up": "scroll up",
+            "wheel_down": "scroll down",
+            "arrow_left": "left arrow",
+            "arrow_left up": "left arrow up",
+            "arrow_up": "up arrow",
+            "arrow_up up": "up arrow up",
+            "arrow_down": "down arrow",
+            "arrow_down up": "down arrow up",
+            "arrow_right": "right arrow",
+            "arrow_right up": "right arrow up",
+            "lcontrol": "left control",
+            "rcontrol": "right control",
+            "lshift": "left shift",
+            "rshift": "right shift",
+            "lalt": "left alt",
+            "ralt": "right alt",
+            "lcontrol up": "left control up",
+            "rcontrol up": "right control up",
+            "lshift up": "left shift up",
+            "rshift up": "right shift up",
+            "lalt up": "left alt up",
+            "ralt up": "right alt up",
+            "control-mouse1": "left mouse down",
+            "control-mouse2": "middle mouse down",
+            "control-mouse3": "right mouse down",
+            "shift-mouse1": "left mouse down",
+            "shift-mouse2": "middle mouse down",
+            "shift-mouse3": "right mouse down",
+            "alt-mouse1": "left mouse down",
+            "alt-mouse2": "middle mouse down",
+            "alt-mouse3": "right mouse down",
+            "page_down": "page down",
+            "page_down up": "page down up",
+            "page_up": "page up",
+            "page_up up": "page up up",
+        }
 
         for e in keyboard_keys:
-            self.accept(f'raw-{e}', self.input, [e, True])
-            self.accept(f'raw-{e}-up', self.input_up, [e, True])
-            self.accept(f'raw-{e}-repeat', self.input_hold, [e, True])
+            self.accept(f"raw-{e}", self.input, [e, True])
+            self.accept(f"raw-{e}-up", self.input_up, [e, True])
+            self.accept(f"raw-{e}-repeat", self.input_hold, [e, True])
 
-        self.accept('buttonDown', self.input)
-        self.accept('buttonUp', self.input_up)
-        self.accept('buttonHold', self.input_hold)
-        self.accept('keystroke', self.text_input)
-        ConfigVariableBool('paste-emit-keystrokes', False)
+        self.accept("buttonDown", self.input)
+        self.accept("buttonUp", self.input_up)
+        self.accept("buttonHold", self.input_hold)
+        self.accept("keystroke", self.text_input)
+        ConfigVariableBool("paste-emit-keystrokes", False)
 
         self.disableMouse()
         mouse._mouse_watcher = self.mouseWatcherNode
@@ -105,26 +184,30 @@ class Ursina(ShowBase):
         application.load_settings()
 
         from ursina.prefabs.hot_reloader import HotReloader
+
         # make sure it's running from a file and not an interactive session.
-        application.hot_reloader = HotReloader(__main__.__file__ if hasattr(__main__, '__file__') else 'None')
+        application.hot_reloader = HotReloader(
+            __main__.__file__ if hasattr(__main__, "__file__") else "None"
+        )
 
         try:
             from ursina import gamepad
         except e as Exception:
             print(e)
 
-        if application.window_type != 'none':
+        if application.window_type != "none":
             window.make_editor_gui()
-        if 'editor_ui_enabled' in kwargs:
-            window.editor_ui.enabled = kwargs['editor_ui_enabled']
-
+        if "editor_ui_enabled" in kwargs:
+            window.editor_ui.enabled = kwargs["editor_ui_enabled"]
 
     def _update(self, task):
         if application.calculate_dt:
-            time.dt = globalClock.getDt() * application.time_scale          # time between frames
+            time.dt = (
+                globalClock.getDt() * application.time_scale
+            )  # time between frames
         mouse.update()
 
-        if hasattr(__main__, 'update') and __main__.update and not application.paused:
+        if hasattr(__main__, "update") and __main__.update and not application.paused:
             __main__.update()
 
         for seq in application.sequences:
@@ -137,50 +220,51 @@ class Ursina(ShowBase):
             if application.paused and entity.ignore_paused == False:
                 continue
 
-            if hasattr(entity, 'update') and callable(entity.update):
+            if hasattr(entity, "update") and callable(entity.update):
                 entity.update()
 
-            if hasattr(entity, 'scripts'):
+            if hasattr(entity, "scripts"):
                 for script in entity.scripts:
-                    if script.enabled and hasattr(script, 'update') and callable(script.update):
+                    if (
+                        script.enabled
+                        and hasattr(script, "update")
+                        and callable(script.update)
+                    ):
                         script.update()
 
         return Task.cont
-
 
     def input_up(self, key, is_raw=False):
         if not is_raw and key in keyboard_keys:
             return
 
-        if key in ('wheel_up', 'wheel_down'):
+        if key in ("wheel_up", "wheel_down"):
             return
 
-        key += ' up'
+        key += " up"
         self.input(key)
 
-
     def input_hold(self, key, is_raw=False):
-        key = key.replace('control-', '')
-        key = key.replace('shift-', '')
-        key = key.replace('alt-', '')
+        key = key.replace("control-", "")
+        key = key.replace("shift-", "")
+        key = key.replace("alt-", "")
 
         if key in self._input_name_changes:
             key = self._input_name_changes[key]
 
-        key += ' hold'
+        key += " hold"
         self.input(key)
-
 
     def input(self, key, is_raw=False):
         if not is_raw and key in keyboard_keys:
             return
 
-        if not 'mouse' in key:
-            for prefix in ('control-', 'shift-', 'alt-'):
+        if not "mouse" in key:
+            for prefix in ("control-", "shift-", "alt-"):
                 if key.startswith(prefix):
-                    key = key.replace('control-', '')
-                    key = key.replace('shift-', '')
-                    key = key.replace('alt-', '')
+                    key = key.replace("control-", "")
+                    key = key.replace("shift-", "")
+                    key = key.replace("alt-", "")
                     if key in keyboard_keys:
                         return
 
@@ -193,7 +277,7 @@ class Ursina(ShowBase):
         input_handler.input(key)
 
         if not application.paused:
-            if hasattr(__main__, 'input'):
+            if hasattr(__main__, "input"):
                 __main__.input(key)
 
         for entity in scene.entities:
@@ -202,30 +286,32 @@ class Ursina(ShowBase):
             if application.paused and entity.ignore_paused == False:
                 continue
 
-            if hasattr(entity, 'input') and callable(entity.input):
+            if hasattr(entity, "input") and callable(entity.input):
                 if entity.input(key):
                     break
 
-            if hasattr(entity, 'scripts'):
+            if hasattr(entity, "scripts"):
                 for script in entity.scripts:
-                    if script.enabled and hasattr(script, 'input') and callable(script.input):
+                    if (
+                        script.enabled
+                        and hasattr(script, "input")
+                        and callable(script.input)
+                    ):
                         if script.input(key):
                             break
 
-
         mouse.input(key)
-
 
     def text_input(self, key):
         key_code = ord(key)
         if key_code < 32 or (key_code >= 127 and key_code <= 160):
             return
 
-        if key == ' ' and input_handler.held_keys['control']:
+        if key == " " and input_handler.held_keys["control"]:
             return
 
         if not application.paused:
-            if hasattr(__main__, 'text_input'):
+            if hasattr(__main__, "text_input"):
                 __main__.text_input(key)
 
         for entity in scene.entities:
@@ -234,17 +320,22 @@ class Ursina(ShowBase):
             if application.paused and entity.ignore_paused == False:
                 continue
 
-            if hasattr(entity, 'text_input') and callable(entity.text_input):
+            if hasattr(entity, "text_input") and callable(entity.text_input):
                 entity.text_input(key)
 
-            if hasattr(entity, 'scripts'):
+            if hasattr(entity, "scripts"):
                 for script in entity.scripts:
-                    if script.enabled and hasattr(script, 'text_input') and callable(script.text_input):
+                    if (
+                        script.enabled
+                        and hasattr(script, "text_input")
+                        and callable(script.text_input)
+                    ):
                         script.text_input(key)
 
-    def step(self):     # use this control the update loop yourself. call app.step() in a while loop for example, instead of app.run()
+    def step(
+        self,
+    ):  # use this control the update loop yourself. call app.step() in a while loop for example, instead of app.run()
         self.taskMgr.step()
-
 
     def run(self, info=True):
         if window.show_ursina_splash:
@@ -252,16 +343,19 @@ class Ursina(ShowBase):
 
         application.load_settings()
         if info:
-            print('os:', platform.system())
-            print('development mode:', application.development_mode)
-            print('application successfully started')
+            print("os:", platform.system())
+            print("development mode:", application.development_mode)
+            print("application successfully started")
 
         super().run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from ursina import *
-    app = Ursina(window_type='offscreen')
+
+    app = Ursina(window_type="offscreen")
+
     def input(key):
         print(key)
+
     app.run()
