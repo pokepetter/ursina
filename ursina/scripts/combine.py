107a01d06e3bde1645aeddfe6c2c099c019a81c9
Petter Amland <petter@rain-games.com>
2023-05-16 17:27:25 +0200

    made combine() take texture_scale and texture_offset into account.

--- ursina/scripts/combine.py
+++ ursina/scripts/combine.py
@@ -52,7 +52,9 @@ def combine(combine_parent, analyze=False, auto_destroy=True, ignore=[]):
             #     norms += e.model.normals
 
             if e.model.uvs:
-                uvs += e.model.uvs
+                uvs.extend([(uv * e.texture_scale) + e.texture_offset for uv in e.model.uvs])
+            else:
+                uvs.extend([(0,0) for e in e.model.vertices])
 
             if e.model.colors: # if has vertex colors
                 cols.extend([Color(*vcol) * e.color for vcol in e.model.colors])
@@ -80,19 +82,21 @@ def combine(combine_parent, analyze=False, auto_destroy=True, ignore=[]):
 
 if __name__ == '__main__':
     from ursina import *
+    from ursina.mesh_exporter import ursina_mesh_to_obj
     app = Ursina()
 
 
-    p = Entity()
+    p = Entity(texture='brick')
     e1 = Entity(parent=p, model='sphere', y=1.5, color=color.pink)
-    e2 = Entity(parent=p, model='cube', color=color.yellow, x=1, origin_y=-.5)
-    e3 = Entity(parent=e2, model='cube', color=color.yellow, y=2, scale=.5)
+    e2 = Entity(parent=p, model='cube', color=color.yellow, x=1, origin_y=-.5, texture='brick')
+    e3 = Entity(parent=e2, model='cube', color=color.yellow, y=2, scale=.5, texture='brick', texture_scale=Vec2(3,3), texture_offset=(.1,.1))
 
     def input(key):
         if key == 'space':
             from time import perf_counter
             t = perf_counter()
             p.combine()
+            p.texture='brick'
             print('combined in:', perf_counter() - t)
 
     # p.y=2
