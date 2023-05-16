1e0119ed8d6bc5af52b740b5741611c35d386207
pokepetter <pokepetter@gmail.com>
2023-05-16 19:59:55 +0200

    fixed raycast.

--- ursina/raycast.py
+++ ursina/raycast.py
@@ -28,7 +28,8 @@ _raycaster._pickerNode.addSolid(_ray)
 
 def raycast(origin, direction:Vec3=(0,0,1), distance=inf, traverse_target:Entity=scene, ignore:list=None, debug=False):
     _ignore = [e for e in scene.entities if not e.collision]
-    _ignore.extend(ignore)
+    if ignore:
+        _ignore.extend(ignore)
 
     _raycaster.position = origin
     _raycaster.look_at(_raycaster.position + direction)
@@ -98,58 +99,59 @@ if __name__ == '__main__':
     Example where we only move if a wall is not hit:
     '''
 
+    #
+    # class Player(Entity):
+    #
+    #     def update(self):
+    #         self.direction = Vec3(
+    #             self.forward * (held_keys['w'] - held_keys['s'])
+    #             + self.right * (held_keys['d'] - held_keys['a'])
+    #             ).normalized()  # get the direction we're trying to walk in.
+    #
+    #         origin = self.world_position + (self.up*.5) # the ray should start slightly up from the ground so we can walk up slopes or walk over small objects.
+    #         hit_info = raycast(origin , self.direction, ignore=(self,), distance=.5, debug=False)
+    #         if not hit_info.hit:
+    #             self.position += self.direction * 5 * time.dt
+    #
+    # Player(model='cube', origin_y=-.5, color=color.orange)
+    # wall_left = Entity(model='cube', collider='box', scale_y=3, origin_y=-.5, color=color.azure, x=-4)
+    # wall_right = duplicate(wall_left, x=4)
+    # camera.y = 2
+    # app.run()
+    #
+    # # test
+    # # breakpoint()
+    # d = Entity(parent=scene, position=(0,0,2), model='cube', color=color.orange, collider='box', scale=2)
+    # e = Entity(model='cube', color=color.lime)
+    #
+    # camera.position = (0, 15, -15)
+    # camera.look_at(e)
+    # # camera.reparent_to(e)
+    # speed = .01
+    # rotation_speed = 1
+    # intersection_marker = Entity(model='cube', scale=.2, color=color.red)
+    #
+    # def update():
+    #     e.position += e.forward * held_keys['w'] * speed
+    #     e.position += e.left * held_keys['a'] * speed
+    #     e.position += e.back * held_keys['s'] * speed
+    #     e.position += e.right * held_keys['d'] * speed
+    #
+    #     e.rotation_y -= held_keys['q'] * rotation_speed
+    #     e.rotation_y += held_keys['e'] * rotation_speed
+    #
+    #     ray = raycast(e.world_position, e.forward, 3, debug=True)
+    #     # ray = raycast(e.world_position, e.forward, 3, debug=True)
+    #     # ray = boxcast(e.world_position, e.right, 3, debug=True)
+    #     # print(ray.distance, ray2.distance)
+    #     intersection_marker.world_position = ray.world_point
+    #     intersection_marker.visible = ray.hit
+    #     if ray.hit:
+    #         d.color = color.azure
+    #     else:
+    #         d.color = color.orange
 
-    class Player(Entity):
-
-        def update(self):
-            self.direction = Vec3(
-                self.forward * (held_keys['w'] - held_keys['s'])
-                + self.right * (held_keys['d'] - held_keys['a'])
-                ).normalized()  # get the direction we're trying to walk in.
-
-            origin = self.world_position + (self.up*.5) # the ray should start slightly up from the ground so we can walk up slopes or walk over small objects.
-            hit_info = raycast(origin , self.direction, ignore=(self,), distance=.5, debug=False)
-            if not hit_info.hit:
-                self.position += self.direction * 5 * time.dt
-
-    Player(model='cube', origin_y=-.5, color=color.orange)
-    wall_left = Entity(model='cube', collider='box', scale_y=3, origin_y=-.5, color=color.azure, x=-4)
-    wall_right = duplicate(wall_left, x=4)
-    camera.y = 2
-    app.run()
-
-    # test
-    # breakpoint()
-    d = Entity(parent=scene, position=(0,0,2), model='cube', color=color.orange, collider='box', scale=2)
-    e = Entity(model='cube', color=color.lime)
-
-    camera.position = (0, 15, -15)
-    camera.look_at(e)
-    # camera.reparent_to(e)
-    speed = .01
-    rotation_speed = 1
-    intersection_marker = Entity(model='cube', scale=.2, color=color.red)
-
-    def update():
-        e.position += e.forward * held_keys['w'] * speed
-        e.position += e.left * held_keys['a'] * speed
-        e.position += e.back * held_keys['s'] * speed
-        e.position += e.right * held_keys['d'] * speed
-
-        e.rotation_y -= held_keys['q'] * rotation_speed
-        e.rotation_y += held_keys['e'] * rotation_speed
-
-        ray = raycast(e.world_position, e.forward, 3, debug=True)
-        # ray = raycast(e.world_position, e.forward, 3, debug=True)
-        # ray = boxcast(e.world_position, e.right, 3, debug=True)
-        # print(ray.distance, ray2.distance)
-        intersection_marker.world_position = ray.world_point
-        intersection_marker.visible = ray.hit
-        if ray.hit:
-            d.color = color.azure
-        else:
-            d.color = color.orange
-
+    ray = raycast(Vec3(0), Vec3(0,0,1), 3, debug=True)
     t = time.time()
     # ray = raycast(e.world_position, e.forward, 3, debug=True)
     print(time.time() - t)
