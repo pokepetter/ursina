7384af6a6eb4a689e1e995d434993adab30bc2c6
pokepetter <pokepetter@gmail.com>
2023-08-20 23:36:56 +0200

    fixed wrong tooltip text color in column graph sample.

--- samples/column_graph.py
+++ samples/column_graph.py
@@ -25,7 +25,7 @@ for i, (name, value) in enumerate(data.items()):
         color = color.color(30*i, 1, .7),
         origin_y = -.5,
         text = name,
-        tooltip = Tooltip('00') # to ensure uniform with
+        tooltip = Tooltip('00', color=color.light_text) # to ensure uniform with
     )
     column.tooltip.text = str(value)
     column.text_entity.scale *= .4
