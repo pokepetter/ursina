39705383e20adc9c155838930c6d0d4ed74bb3ad
pokepetter <pokepetter@gmail.com>
2023-09-06 20:19:14 +0200

    added popup option for ButtonList.

--- ursina/prefabs/button_list.py
+++ ursina/prefabs/button_list.py
@@ -1,18 +1,22 @@
-from ursina import Entity, Text, camera, Button, color, mouse, Sequence
+from ursina import Entity, Text, camera, Button, color, mouse, Sequence, Vec3
 from math import floor
 
 
 class ButtonList(Entity):
-    def __init__(self, button_dict, button_height=1.1, width=.5, font=Text.default_font, **kwargs):
+    def __init__(self, button_dict, button_height=1.1, width=.5, popup=False, color=Button.default_color, highlight_color=color.white33, selected_color=color.azure, font=Text.default_font, **kwargs):
         super().__init__(parent=camera.ui, position=(-(width/2), .45))
 
         self.button_height = button_height
         self.width = width
         self.text_entity = Text(parent=self, font=font, origin=(-.5,.5), text='empty', world_scale=20, z=-.1, x=.01, y=-(button_height*.25*Text.size), line_height=button_height)
-        self.bg = Entity(parent=self, model='quad', origin=(-.5,.5), scale=width, color=Button.color, collider='box')
-        self.highlight = Entity(parent=self.bg, model='quad', color=color.white33, scale=(1,self.button_height), origin=(-.5,.5), z=-.01, add_to_scene_entities=False)
-        self.selection_marker = Entity(parent=self.bg, model='quad', color=color.azure, scale=(1,self.button_height), origin=(-.5,.5), z=-.02, enabled=False, add_to_scene_entities=False)
+        self.bg = Entity(parent=self, model='quad', origin=(-.5,.5), scale=width, color=color, collider='box')
+        self.highlight = Entity(parent=self.bg, model='quad', color=highlight_color, scale=(1,self.button_height), origin=(-.5,.5), z=-.01, add_to_scene_entities=False)
+        self.selection_marker = Entity(parent=self.bg, model='quad', color=selected_color, scale=(1,self.button_height), origin=(-.5,.5), z=-.02, enabled=False, add_to_scene_entities=False)
         self.button_dict = button_dict
+
+        self.popup = popup
+        if self.popup:
+            self.close_button = Entity(parent=self, scale=Vec3(100,100,.1), model='quad', collider='box', visible_self=False, on_click=self.disable)
 
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -27,6 +31,10 @@ class ButtonList(Entity):
         self._button_dict = value
         self.actions = list(self._button_dict.values())
         self.bg.scale_y = self.button_height * len(value) * Text.size
+        # self.bg.model='circle'
+        self.bg.model = 'quad'
+        self.bg.origin = self.bg.origin
+
         self.text_entity.text = '\n'.join(self.button_dict.keys())
         self.text_entity.line_height = self.button_height
         self.highlight.scale_y = 1 / len(value)
@@ -40,7 +48,6 @@ class ButtonList(Entity):
             y = min(y, len(self.button_dict)-1)
 
             action = self.actions[y]
-            self.highlight.blink(color.black, .1)
             self.selection_marker.enabled = True
             self.selection_marker.y = self.highlight.y
 
@@ -49,6 +56,9 @@ class ButtonList(Entity):
 
             elif isinstance(action, Sequence):
                 action.start()
+
+            if self.popup:
+                self.disable()
 
         if key == 'left mouse down' and not self.bg.hovered:
             self.selection_marker.enabled = False
@@ -81,7 +91,7 @@ if __name__ == '__main__':
     for i in range(6, 20):
         button_dict[f'button {i}'] = Func(print, i)
 
-    bl = ButtonList(button_dict, font='VeraMono.ttf', button_height=1.5)
+    bl = ButtonList(button_dict, font='VeraMono.ttf', button_height=1.5, popup=True)
     def input(key):
         if key == 'space':
             bl.button_dict = {
