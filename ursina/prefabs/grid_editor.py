2c1cc2241161910e9cf6696ea6eab642152f1d4d
Petter Amland <petter@rain-games.com>
2023-09-26 18:29:26 +0200

    added rect selection tool and copy paste to PixelEditor.

--- ursina/prefabs/grid_editor.py
+++ ursina/prefabs/grid_editor.py
@@ -1,9 +1,12 @@
 from ursina import *
 import pyperclip
+from PIL import Image
 from copy import deepcopy
 import sys
 from math import floor
 from ursina.shaders import unlit_shader
+import json
+
 
 from ursina.scripts.property_generator import generate_properties_for_class
 @generate_properties_for_class()
@@ -11,7 +14,8 @@ class GridEditor(Entity):
     def __init__(self, size=(32,32), palette=(' ', '#', '|', 'o'), canvas_color=color.white, **kwargs):
         super().__init__(parent=camera.ui, position=(-.45,-.45), scale=.9, model='quad', origin=(-.5,-.5), visible_self=False)
         self.w, self.h = int(size[0]), int(size[1])
-        self.canvas = Entity(parent=self, model='quad', origin=(-.5,-.5), collider='box', shader=unlit_shader, scale=(self.w/self.h, 1), color=canvas_color)
+        self.canvas = Entity(parent=self, model='quad', origin=(-.5,-.5), shader=unlit_shader, scale=(self.w/self.h, 1), color=canvas_color)
+        self.canvas_collider = Entity(parent=self.canvas, model='wireframe_quad', origin=self.canvas.origin, color=color.blue, scale=2, position=(-.5,-.5), collider='box')
         sys.setrecursionlimit(max(sys.getrecursionlimit(), self.w * self.h))
         # self.grid = [[palette[0] for x in range(self.w)] for y in range(self.h)]
         if not hasattr(self, 'grid'):
@@ -27,14 +31,20 @@ class GridEditor(Entity):
         self.lock_axis = None
         self.outline = Entity(parent=self.canvas, model=Quad(segments=0, mode='line', thickness=2), color=color.cyan, z=.01, origin=(-.5,-.5))
 
-        self.selection = [Vec2(0,0), Vec2(0,0)]
-        self.selection_renderer = Entity(parent=self.canvas, model=Quad(segments=0, mode='line', thickness=2), color=color.lime, z=.01, origin=(-.5,-.5), enabled=False)
+        self.rect_selection = [Vec2(0,0), Vec2(0,0)]
+        self.selection_renderer = Entity(parent=self.canvas, model=Mesh(mode='line', thickness=2), color=color.lime, alpha=.5, z=-.01, origin=(-.5,-.5), scale=(1/self.w,1/self.h))
+        self.rect_tool = Entity(parent=self.canvas, model=Quad(0, mode='line', thickness=2), color=color.lime, z=-.01, origin=(-.5,-.5), start=Vec2(0,0), end=Vec2(0,0))
+        # self.selection_mover = Draggable(parent=self.canvas, model='circle', color=color.blue, origin=(.5,.5), step=(1/self.w,1/self.h,0), enabled=False)
+        self.selection_matrix = [[0 for y in range(self.h)] for x in range(self.w)]
+        self.temp_paste_layer = Entity(parent=self.cursor, model='quad', origin=(-.5,-.5), z=-.02, enabled=1)
+        Entity(parent=self.temp_paste_layer, model='wireframe_quad', origin=self.temp_paste_layer.origin, color=color.black)
+        self.is_in_paste_mode = False
 
         self.undo_stack = []
         self.undo_stack.append(deepcopy(self.grid))
         self.undo_index = 0
 
-        self.help_icon = Button(parent=self, scale=.025, model='circle', origin=(-.5,-.5), position=(-.0,1.005,-1), text='?')
+        self.help_icon = Button(parent=self.canvas, scale=.025, model='circle', origin=(-.5,-.5), position=(-.0,1.005,-1), text='?', target_scale=.025)
         self.help_icon.tooltip = Tooltip(
             text=dedent('''
                 left mouse:    draw
@@ -88,9 +98,9 @@ class GridEditor(Entity):
         if not self.edit_mode:
             return
 
-        self.cursor.enabled = mouse.hovered_entity == self.canvas
-        if self.canvas.hovered:
-            self.cursor.position = mouse.point
+        self.cursor.enabled = self.canvas_collider.hovered
+        if self.canvas_collider.hovered:
+            self.cursor.position = mouse.point*2 - Vec3(.5,.5,0)
 
             # center cursor
             self.cursor.x -= self.brush_size // 2 / self.w
@@ -117,9 +127,10 @@ class GridEditor(Entity):
 
                 y = int(round(self.cursor.y * self.h))
                 x = int(round(self.cursor.x * self.w))
+                if x < 0 or x > self.w-1 or y < 0 or y > self.h-1:
+                    return
 
-
-                if not held_keys['alt']:
+                if not held_keys['alt'] and not self.is_in_paste_mode:
                     if self.prev_draw is not None and distance_2d(self.prev_draw, (x,y)) > 1:
                         dist = distance_2d(self.prev_draw, (x,y))
 
@@ -139,14 +150,28 @@ class GridEditor(Entity):
                     self.selected_char = self.grid[x][y]
 
         if mouse.right:     # selection
-            self.selection[1] = self.cursor.position
-            if self.selection[0] == self.selection[1]:
-                self.selection_renderer.enabled = False
+            self.rect_selection[1] = self.get_cursor_position()
+            if self.rect_selection[0] == self.rect_selection[1]:
+                self.rect_tool.start = Vec2(0,0)
+                self.rect_tool.end = Vec2(0,0)
+                self.rect_tool.enabled = False
                 return
-            self.selection_renderer.enabled = True
-            self.selection_renderer.position = Vec3(min(self.selection[0].x, self.selection[1].x), min(self.selection[0].y, self.selection[1].y), -.1)
-            end = Vec3(max(self.selection[0].x, self.selection[1].x), max(self.selection[0].y, self.selection[1].y), -.1) + Vec3(1/self.w,1/self.h,0)
-            self.selection_renderer.scale = end - self.selection_renderer.position
+
+            self.rect_tool.start = Vec2(min(self.rect_selection[0].x, self.rect_selection[1].x), min(self.rect_selection[0].y, self.rect_selection[1].y))
+            self.rect_tool.end =   Vec2(max(self.rect_selection[0].x, self.rect_selection[1].x)+1, max(self.rect_selection[0].y, self.rect_selection[1].y)+1)
+            self.rect_tool.start.x = clamp(self.rect_tool.start.x, 0, self.w)
+            self.rect_tool.start.y = clamp(self.rect_tool.start.y, 0, self.h)
+            self.rect_tool.end.x = clamp(self.rect_tool.end.x, 0, self.w)
+            self.rect_tool.end.y = clamp(self.rect_tool.end.y, 0, self.h)
+            self.rect_tool.enabled = True
+            self.rect_tool.position = self.rect_tool.start / Vec2(self.w, self.h)
+            self.rect_tool.scale = (self.rect_tool.end - self.rect_tool.start) / Vec2(self.w, self.h)
+
+
+    def get_cursor_position(self):
+        y = int(round(self.cursor.y * self.h))
+        x = int(round(self.cursor.x * self.w))
+        return Vec2(x,y)
 
 
     def draw(self, x, y):
@@ -165,8 +190,8 @@ class GridEditor(Entity):
         if not self.edit_mode:
             return
 
-        if key == 'left mouse down':
-            self.start_pos = self.cursor.position
+        if key == 'left mouse down' and not self.is_in_paste_mode:
+            self.start_pos = self.get_cursor_position()
             if not held_keys['control']:
                 self.prev_draw = None
 
@@ -178,8 +203,38 @@ class GridEditor(Entity):
             if not held_keys['control']:
                 self.record_undo()
 
-        if key == 'right mouse down':
-            self.selection[0] = self.cursor.position
+        if key == 'right mouse down' and not self.is_in_paste_mode:
+            if not held_keys['shift'] and not held_keys['alt']:
+                self.selection_mode = 'overwrite'
+                self.clear_selection()
+            elif (held_keys['control'] or held_keys['shift']) and not held_keys['alt']:
+                self.selection_mode = 'add'
+            elif held_keys['alt'] and not (held_keys['control'] or held_keys['shift']):
+                self.selection_mode = 'subtract'
+
+            self.rect_selection[0] = self.get_cursor_position()
+
+
+        if key == 'left mouse down' and self.is_in_paste_mode:
+            self.exit_paste_mode()
+
+
+        if key == 'right mouse down' and self.is_in_paste_mode:
+            self.exit_paste_mode(discard=True)
+
+
+        elif key == 'right mouse up':
+            self.rect_tool.enabled = False
+            # if self.selection_mode == 'overwrite':
+            #     self.clear_selection()
+
+            new_value = int(self.selection_mode == 'overwrite' or self.selection_mode == 'add')
+            for y in range(self.rect_tool.start.Y, self.rect_tool.end.Y):
+                for x in range(self.rect_tool.start.X, self.rect_tool.end.X):
+                    self.selection_matrix[x][y] = new_value
+
+            self.render_selection()
+
 
         if key == 'shift up':
             self._lock_origin = None
@@ -197,7 +252,7 @@ class GridEditor(Entity):
             self.render()
 
         # fill
-        if key == 'g' and self.canvas.hovered:
+        if key == 'g' and self.canvas_collider.hovered:
             y = int(self.cursor.y * self.h)
             x = int(self.cursor.x * self.w)
             self.floodfill(self.grid, x, y)
@@ -217,6 +272,12 @@ class GridEditor(Entity):
         if held_keys['control'] and key == 's':
             if hasattr(self, 'save'):
                 self.save()
+
+        if held_keys['control'] and key == 'c':
+            self.copy()
+
+        if held_keys['control'] and key == 'v':
+            self.enter_paste_mode()
 
 
     def record_undo(self):
@@ -245,7 +306,106 @@ class GridEditor(Entity):
             if y < self.h-1:
                 self.floodfill(matrix, x, y+1, first=False)
 
+    def copy(self):
+        # crop matrix
+        # find the indices of the rows and columns with True values
+        rows = [i for i in range(len(self.selection_matrix)) if any(self.selection_matrix[i])]
+        cols = [j for j in range(len(self.selection_matrix[0])) if any(self.selection_matrix[i][j] for i in range(len(self.selection_matrix)))]
 
+        # crop the matrix based on the boolean values
+        start_x = min(rows)
+        start_y = min(cols)
+        # end_x = max(rows) + 1
+        # end_y = max(cols) + 1
+        selection_width =  (max(rows) + 1) - start_x
+        selection_height = (max(cols) + 1) - start_y
+        # cropped_matrix = [[matrix[i][j] for j in range(start_y, max(cols) + 1)] for i in range(start_x, max(rows) + 1)]
+
+        copy_data = [[tuple(self.grid[start_x+x][start_y+y]) if self.selection_matrix[x][y] else None for y in range(selection_height)] for x in range(selection_width)]
+        pyperclip.copy(json.dumps(dict(data=copy_data)))
+
+
+    def enter_paste_mode(self):
+        self.is_in_paste_mode = True
+        self.paste_data = json.loads(pyperclip.paste())
+        if not self.paste_data['data']:
+            return
+        self.paste_data = self.paste_data['data']
+        w = len(self.paste_data)
+        h = len(self.paste_data[1])
+
+        self.temp_paste_layer.enabled = True
+        self.temp_paste_layer.scale = (w,h)
+
+        if not self.temp_paste_layer.texture or self.temp_paste_layer.texture.size != (w,h):
+            self.temp_paste_layer.texture = Texture(Image.new(mode='RGBA', size=(w,h), color=(0,0,0,0)))
+
+        for x in range(w):
+            for y in range(h):
+                pixel = self.paste_data[x][y]
+                if not pixel:
+                    pixel = color.clear
+
+                self.temp_paste_layer.texture.set_pixel(x, y, pixel)
+        self.temp_paste_layer.texture.apply()
+
+
+    def exit_paste_mode(self, discard=False):
+        self.is_in_paste_mode = False
+        self.temp_paste_layer.enabled = False
+        x_offset = int(self.cursor.x * self.w)
+        y_offset = int(self.cursor.y * self.h)
+        w = len(self.paste_data)
+        h = len(self.paste_data[1])
+
+        if discard:
+            return
+
+        for x in range(w):
+            for y in range(h):
+                real_x = x + x_offset
+                real_y = y + y_offset
+
+                if real_x < 0 or real_x >= self.w or real_y < 0 or real_y >= self.h:
+                    continue
+
+                pixel = self.paste_data[x][y]
+                if not pixel:
+                    continue
+
+                self.grid[x+x_offset][y+y_offset] = pixel
+
+        self.render()
+
+
+    def clear_selection(self):
+        self.selection_matrix = [[0 for y in range(self.h)] for x in range(self.w)]
+        self.selection_renderer.model.clear()
+        print('cleare selection')
+        # print('seofijsoefisjeofij', self.selection_renderer.enabled)
+
+
+    def render_selection(self):
+        quad = load_model('quad', use_deepcopy=True)
+        # self.selection_renderer.enabled = True
+        self.selection_renderer.model.clear(False)
+        verts = []
+        for x in range(self.w):
+            for y in range(self.h):
+                if self.selection_matrix[x][y]:
+                    if x <= 0 or not self.selection_matrix[x-1][y]:
+                        verts.extend((Vec3(x,y,0), Vec3(x,y+1,0)))
+                    if x >= self.w-1 or not self.selection_matrix[x+1][y]:
+                        verts.extend((Vec3(x+1,y,0), Vec3(x+1,y+1,0)))
+                    if y <= 0 or not self.selection_matrix[x][y-1]:
+                        verts.extend((Vec3(x,y,0), Vec3(x+1,y,0)))
+                    if y >= self.h-1 or not self.selection_matrix[x][y+1]:
+                        verts.extend((Vec3(x,y+1,0), Vec3(x+1,y+1,0)))
+
+        # if verts:
+        self.selection_renderer.model.vertices = [v+Vec3(-.5,-.5,0) for v in verts]
+        self.selection_renderer.model.triangles = [(i, i+1) for i in range(0, len(verts), 2)]
+        self.selection_renderer.model.generate()
 
 
 class PixelEditor(GridEditor):
@@ -260,6 +420,8 @@ class PixelEditor(GridEditor):
         self.grid = [[texture.get_pixel(x,y) for y in range(texture.height)] for x in range(texture.width)]
         self.canvas.texture.filtering = None
         self.cursor.scale = Vec2(self.brush_size / self.w, self.brush_size / self.h)
+        self.help_icon.scale = self.help_icon.target_scale
+        # self.selection_mover.world_scale = .5
 
         if clear_undo_stack:
             self.undo_stack.clear()
@@ -337,7 +499,7 @@ class ASCIIEditor(GridEditor):
 
 
 if __name__ == '__main__':
-    app = Ursina(size=(1920,1080))
+    app = Ursina(borderless=False)
     '''
     pixel editor example, it's basically a drawing tool.
     can be useful for level editors and such
@@ -346,12 +508,19 @@ if __name__ == '__main__':
     from PIL import Image
     t = Texture(Image.new(mode='RGBA', size=(32,32), color=(0,0,0,1)))
     from ursina.prefabs.grid_editor import PixelEditor
-    PixelEditor(texture=load_texture('brick'), x=-.7, scale=.6)
+    editor = PixelEditor(parent=scene, texture=load_texture('brick'), scale=10)
 
-    '''
-    same as the pixel editor, but with text.
-    '''
-    from ursina.prefabs.grid_editor import ASCIIEditor
-    ASCIIEditor(x=0, scale=.1)
+    camera.orthographic = True
+    camera.fov = 15
+    EditorCamera(rotation_speed=0)
+    # editor.selection_mover.enabled = True
+    # editor.selection_mover.on_click()
+    # editor.temp_layer.alpha = .5
+
+    # '''
+    # same as the pixel editor, but with text.
+    # '''
+    # from ursina.prefabs.grid_editor import ASCIIEditor
+    # ASCIIEditor(x=0, scale=.1)
 
     app.run()
