0d8878b4f2fdf0f36ecbf178b190903f8d6089fe
pokepetter <pokepetter@gmail.com>
2023-11-11 14:12:37 +0100

    changed triplanar shader input names.

--- ursina/shaders/triplanar_shader.py
+++ ursina/shaders/triplanar_shader.py
@@ -34,14 +34,12 @@ in vec2 texcoord;
 out vec4 fragColor;
 
 uniform sampler2D p3d_Texture0;
-uniform sampler2D top_texture;
+uniform sampler2D side_texture;
 in vec3 world_normal;
 in vec3 vertex_world_position;
 
 uniform vec2 texture_scale;
-uniform vec2 top_texture_scale;
-in vec2 normalRepeat;
-in vec2 normalScale;
+uniform vec2 side_texture_scale;
 
 in vec4 vertex_color;
 
@@ -77,12 +75,12 @@ void main(){
     vec3 blendFast = TriPlanarBlendWeightsConstantOverlap(world_normal);
     vec3 blend = blendFast;
 
-    vec3 albedoX = texture(p3d_Texture0, vertex_world_position.zy * texture_scale).rgb*blend.x;
-	vec3 albedoY = texture(p3d_Texture0, vertex_world_position.xz * texture_scale).rgb*blend.y;
-	vec3 albedoZ = texture(p3d_Texture0, vertex_world_position.xy * texture_scale).rgb*blend.z;
+    vec3 albedoX = texture(side_texture, vertex_world_position.zy * side_texture_scale).rgb*blend.x;
+	vec3 albedoY = texture(side_texture, vertex_world_position.xz * side_texture_scale).rgb*blend.y;
+	vec3 albedoZ = texture(side_texture, vertex_world_position.xy * side_texture_scale).rgb*blend.z;
 
     if (world_normal.y > .0) {
-		albedoY = texture(top_texture, vertex_world_position.xz * top_texture_scale.xy).rgb*blend.y;
+		albedoY = texture(p3d_Texture0, vertex_world_position.xz * texture_scale.xy).rgb*blend.y;
     }
 
 	vec3 triPlanar = (albedoX + albedoY + albedoZ);
@@ -94,10 +92,8 @@ void main(){
 geometry='',
 default_input = {
     'texture_scale' : Vec2(1,1),
-    'top_texture_scale' : Vec2(1,1),
-    'normalRepeat' : Vec2(10,10),
-    'normalScale' : Vec2(1,1),
-    'top_texture' : Func(load_texture, 'grass'),
+    'side_texture' : 'brick',
+    'side_texture_scale' : Vec2(1,1),
     }
 )
 
@@ -113,28 +109,28 @@ if __name__ == '__main__':
     shader = triplanar_shader
 
     a = Draggable(parent=scene, model='cube', shader=shader, texture=load_texture('brick'), plane_direction=Vec3(0,1,0))
-    a.model.uvs= []
-    a.model.generate()
+    # a.model.uvs= []
+    # a.model.generate()
     # a.model.setTexture(a.texture._texture, 1)
     t = load_texture('brick')._texture
     print('------', type(t))
-    a.set_shader_input('top_texture', load_texture('grass'))
+    a.set_shader_input('side_texture', load_texture('brick'))
     # print('---------', a.texture._texture)
 
-    b = AzureSphere(shader=shader, rotation_y=180, x=3, texture='brick')
+    b = AzureSphere(shader=shader, rotation_y=180, x=3, texture='grass')
     b.texture.filtering = False
     GrayPlane(scale=10, y=-2, texture='shore')
-    b.set_shader_input('top_texture', load_texture('grass'))
+    b.set_shader_input('side_texture', load_texture('brick'))
 
     Sky(color=color.light_gray)
     EditorCamera()
 
-    def set_top_texture_scale():
-        value = top_texture_scale_slider.value
-        b.set_shader_input('top_texture_scale', Vec2(value, value))
-        a.set_shader_input('top_texture_scale', Vec2(value, value))
+    def set_side_texture_scale():
+        value = side_texture_scale_slider.value
+        b.set_shader_input('side_texture_scale', Vec2(value, value))
+        a.set_shader_input('side_texture_scale', Vec2(value, value))
 
-    top_texture_scale_slider = Slider(text='top_texture_scale', min=0, max=10, default=1, dynamic=True, on_value_changed=set_top_texture_scale)
+    side_texture_scale_slider = Slider(text='side_texture_scale', min=0, max=10, default=1, dynamic=True, on_value_changed=set_side_texture_scale)
 
 
     def update():
