95e3d3d6f9cccf3c863ae752108042f954338d73
pokepetter <pokepetter@gmail.com>
2024-02-25 14:18:56 +0100

    made DirecitonalLight not include Sky when finding shadow area.

--- ursina/prefabs/sky.py
+++ ursina/prefabs/sky.py
@@ -1,18 +1,21 @@
 from ursina import *
 
 class Sky(Entity):
+    instances = []
 
     def __init__(self, **kwargs):
         from ursina.shaders import unlit_shader
-        super().__init__(parent=camera, name='sky', model='sky_dome', texture='sky_default', scale=9900, shader=unlit_shader)
+        super().__init__(parent=camera, name='sky', model='sky_dome', texture='sky_default', scale=9900, shader=unlit_shader, unlit=True)
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+        __class__.instances.append(self)
+
 
     def update(self):
         self.world_rotation = Vec3(0,0,0)
-        self.scale = camera.clip_plane_far * .95
+        self.scale = camera.clip_plane_far * .9
 
 if __name__  == '__main__':
     app = Ursina()
