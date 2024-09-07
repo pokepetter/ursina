ccd38fea38030fdd04ff7cf6ccc8ff342c50e5db
pokepetter <pokepetter@gmail.com>
2024-09-07 13:17:59 +0200

    made HealthBar bar update rounded corners correctly after changing scale.

--- ursina/prefabs/health_bar.py
+++ ursina/prefabs/health_bar.py
@@ -4,11 +4,10 @@ from ursina.scripts.property_generator import generate_properties_for_class
 @generate_properties_for_class()
 class HealthBar(Button):
 
-    def __init__(self, max_value=100, value=Default, roundness=.25, animation_duration=.1, show_text=True, show_lines=False, text_size=.7, origin=(-.5,.5), **kwargs):
-        super().__init__(position=(-.45*window.aspect_ratio,.45), scale=(Text.size*20,Text.size), origin=origin,
-        color=color.black66, highlight_color=color.black66, text='hp / max hp', text_size=text_size, radius=roundness, ignore=True)
+    def __init__(self, max_value=100, value=Default, roundness=.25, bar_color=color.red.tint(-.2), highlight_color=color.black66, animation_duration=.1, show_text=True, show_lines=False, text_size=.7, scale=(.5,.025), origin=(-.5,.5), name='health_bar', **kwargs):
+        super().__init__(name=name, position=(-.45*window.aspect_ratio,.45), origin=origin, scale=scale, text='hp / max hp', text_size=text_size, radius=roundness, ignore=True)
 
-        self.bar = Entity(parent=self, model=Quad(radius=roundness), origin=origin, z=-.005, color=color.red.tint(-.2), ignore=True)
+        self.bar = Entity(parent=self, model=Quad(radius=roundness), origin=origin, z=-.005, color=bar_color, highlight_color=highlight_color, ignore=True)
         self.lines = Entity(parent=self.bar, y=-1, color=color.black33, ignore=True, enabled=show_lines, z=-.05)
 
         self.max_value = max_value
@@ -65,9 +64,14 @@ class HealthBar(Button):
 
 
     def __setattr__(self, name, value):
-        if 'scale' and hasattr(self, 'model') and self.model:  # update rounded corners of background when scaling
+        if name == 'scale' and hasattr(self, 'model') and self.model:  # update rounded corners of background when scaling
             self.model.aspect = self.world_scale_x / self.world_scale_y
+            # print(self.name, 'aaaaaaaaaaaaa updat scale', self.scale)
             self.model.generate()
+            if hasattr(self, 'bar'):
+                self.bar.model = Quad(aspect=self.world_scale_x/self.world_scale_y, radius=self.roundness)
+                self.bar.origin = self.origin
+            
             if hasattr(self, 'text_entity') and self.text_entity:
                 self.text_entity.world_scale = 25 * self.text_size
 
@@ -77,7 +81,7 @@ class HealthBar(Button):
 if __name__ == '__main__':
     app = Ursina()
 
-    health_bar_1 = HealthBar(bar_color=color.lime.tint(-.25), roundness=.5, max_value=100, value=50)
+    health_bar_1 = HealthBar(bar_color=color.lime.tint(-.25), roundness=.5, max_value=100, value=50, scale=(.5,.1))
     print(health_bar_1.text_entity.enabled, health_bar_1.text_entity.text)
     # health_bar_1.show_text = False
     # health_bar_1.show_lines = True
