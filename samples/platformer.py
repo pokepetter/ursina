575b12a57d88d998f12823d72dcdd623ca2eb27c
pokepetter <pokepetter@gmail.com>
2025-05-01 23:25:47 +0200

    updated samples, made a split between single file projects and multi-file projects, the latter which will link to github. added protein visualization sample. made documentation generator produce valid samples for sswg v4.1.0.

--- samples/platformer.py
+++ samples/platformer.py
@@ -1,11 +1,6 @@
-import time
-# t = time.time()
-
 from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
-# window.vsync = False
-window.borderless = False
 app = Ursina()
 window.color = color.light_gray
 camera.orthographic = True
@@ -15,15 +10,11 @@ ground = Entity(model='cube', color=color.olive.tint(-.4), z=-.1, y=-1, origin_y
 random.seed(4)
 for i in range(10):
     Entity(model='cube', color=color.dark_gray, collider='box', ignore=True, position=(random.randint(-20,20), random.randint(0,10)), scale=(random.randint(1,20), random.randint(2,5), 10))
-# ground = Entity(model='cube', color=color.white33, origin_y=.5, scale=(20, 10, 1), collider='box')
-# wall = Entity(model='cube', color=color.azure, origin=(-.5,.5), scale=(5,10), x=10, y=.5, collider='box')
-# ceiling = Entity(model='cube', color=color.white33, origin_y=.5, scale=(10, 1, 1), y=4, collider='box')
 
 player = PlatformerController2d()
-player.x=1
+player.x = 1
 player.y = raycast(player.world_position, player.down).world_point[1] + .01
 camera.add_script(SmoothFollow(target=player, offset=[0,5,-30], speed=4))
-
 
 
 input_handler.bind('right arrow', 'd')
@@ -33,8 +24,8 @@ input_handler.bind('gamepad dpad right', 'd')
 input_handler.bind('gamepad dpad left', 'a')
 input_handler.bind('gamepad a', 'space')
 
-# test
-from ursina.scripts.noclip_mode import NoclipMode2d
-player.add_script(NoclipMode2d())
+if application.development_mode:    # cheats
+    from ursina.scripts.noclip_mode import NoclipMode2d
+    player.add_script(NoclipMode2d())
 
 app.run()
