083e981c2da8587ebf293396618ca1b9d38e99a1
pokepetter <pokepetter@gmail.com>
2025-12-11 11:44:50 +0100

    oops.

--- ursina/entity.py
+++ ursina/entity.py
@@ -792,8 +792,6 @@ class Entity(NodePath, metaclass=PostInitCaller):
     #     return self._shader
 
     def shader_setter(self, value):
-        self._shader = value
-        return
         from ursina import camera
         if hasattr(camera, 'ui') and self.has_ancestor(camera.ui) and self.shader == unlit_with_fog_shader:
             value = unlit_shader
